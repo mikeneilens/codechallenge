09bebc47ccda01069182176f4349ae908dc0040b
Mike Neilens <mike.neilens@gmail.com>
2019-03-14 08:24:25 +0000

    added logic for single values

--- formatTimekt/src/main/kotlin/Main.kt
+++ formatTimekt/src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
 package mike
 fun main(args: Array<String>) {
-    print(formatTime(0))
+    println(formatTime(0))
+    println(formatTime(1))
+    println(formatTime(60))
+    println(formatTime(3600))
+    println(formatTime(86400))
+    println(formatTime(3156000))
 }
-fun formatTime(timeInSeconds:Int):String{
-    return "none"
+fun formatTime(timeInSeconds:Int)  = when (timeInSeconds) {
+    0 -> "none"
+    1 -> "1 second"
+    60 -> "1 minute"
+    3600 -> "1 hour"
+    86400 -> "1 day"
+    31536000 -> "1 year"
+    else -> "none"
 }
--- formatTimekt/src/test/kotlin/MyTests.kt
+++ formatTimekt/src/test/kotlin/MyTests.kt
@@ -6,6 +6,19 @@ class MyTests {
     @Test
     fun singleValues() {
         assertEquals("none",formatTime(0))
+        assertEquals("1 second",formatTime(1))
+        assertEquals("1 minute",formatTime(60))
+        assertEquals("1 hour",formatTime(3600))
+        assertEquals("1 day",formatTime(86400))
+        assertEquals("1 year",formatTime(31536000))
+    }
+    @Test
+    fun pluralValues() {
+        assertEquals("2 seconds",formatTime(2))
+        assertEquals("2 minutes",formatTime(120))
+        assertEquals("2 hours",formatTime(7200))
+        assertEquals("2 days",formatTime(172800))
+        assertEquals("2 years",formatTime(63072000))
     }
 }
 
