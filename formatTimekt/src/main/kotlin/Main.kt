447e4f058910ee51d0920b3e3a6663c9a7fe4922
Mike Neilens <mike.neilens@gmail.com>
2019-03-14 18:31:16 +0000

    Refactor to create separate function to calculate values

--- formatTimekt/src/main/kotlin/Main.kt
+++ formatTimekt/src/main/kotlin/Main.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>) {
     println(formatTime(86400))
     println(formatTime(3156000))
 }
-val conversions = listOf(
+val quantitiesInEachUnit = listOf(
       UnitTime("year",31536000)
     , UnitTime("day", 86400)
     , UnitTime("hour",3600)
@@ -18,16 +18,22 @@ val conversions = listOf(
 fun formatTime(timeInSeconds:Int): String {
     if (timeInSeconds == 0) return "none"
 
-    var remainingTime = timeInSeconds
 
-    val convertUnit = fun  (unitTime:UnitTime):UnitTime {
-        val newValue = remainingTime / unitTime.value
-        remainingTime = remainingTime % unitTime.value
-        return UnitTime(unitTime.unit, timeInSeconds / unitTime.value)
-    }
-    val listOfUnitTimes:List<UnitTime> = conversions.map(convertUnit)
+    val listOfUnitTimes = createListOfUnitTimes(timeInSeconds)
 
     val nonZeroUnitTImes = listOfUnitTimes.filter { unitTime:UnitTime -> unitTime.value > 0 }
 
     return nonZeroUnitTImes[0].toString()
- }
+}
+
+fun createListOfUnitTimes(timeInSeconds: Int):List<UnitTime> {
+    var remainingTime = timeInSeconds
+
+    val calcValueForEachUnit = fun  (unitTime:UnitTime):UnitTime {
+        val newValue = remainingTime / unitTime.value
+        remainingTime = remainingTime % unitTime.value
+        return UnitTime(unitTime.unit, timeInSeconds / unitTime.value)
+    }
+
+    return quantitiesInEachUnit.map(calcValueForEachUnit)
+}
