fcd0e15492d91c961399dec1c744e60eea41bbbc
Mike Neilens <mike.neilens@gmail.com>
2019-03-29 17:35:03 +0000

    Fixed tests

--- challenge2/src/test/kotlin/Tests.kt
+++ challenge2/src/test/kotlin/Tests.kt
@@ -6,17 +6,25 @@ class MyTests {
     fun `Single valid price on input`() {
         assertEquals("now £100",fixPriceLabel("now £100"))
      }
+    @Test
     fun `Two valid prices on input`() {
-        assertEquals("Was £200, now £100",fixPriceLabel("Now £100"))
+        assertEquals("Was £200, now £100",fixPriceLabel("Was £200, now £100"))
     }
+    @Test
     fun `Three valid prices on input`() {
-        assertEquals("Was £200, then £150, now £100",fixPriceLabel("Now £100"))
+        assertEquals("Was £200, then £150, now £100",fixPriceLabel("Was £200, then £150, now £100"))
     }
+    @Test
     fun `Four valid prices on input`() {
-        assertEquals("Was £200, then £150, now £100",fixPriceLabel("Now £100"))
+        assertEquals("Was £200, then £150, now £100",fixPriceLabel("Was £200, then £150, now £100"))
     }
+    @Test
     fun `Three valid prices with some decimal`() {
-        assertEquals("Was £200, then £150.00, now £100.50",fixPriceLabel("Now £100"))
+        assertEquals("Was £200, then £150.00, now £100.50",fixPriceLabel("Was £200, then £150.00, now £100.50"))
+    }
+    @Test
+    fun `Now price is higher than the Was price`() {
+        assertEquals("Was £200, now £250",fixPriceLabel("now £2500"))
     }
  }
 
