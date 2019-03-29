2e9ad4fda54e98b8947f97236da410c6b32206ce
Mike Neilens <mike.neilens@gmail.com>
2019-03-29 19:28:11 +0000

    refactored to remove invalid prices recursively

--- challenge2/src/main/kotlin/Main.kt
+++ challenge2/src/main/kotlin/Main.kt
@@ -1,42 +1,39 @@
 fun main(args: Array<String>) {
-
+    fixPriceLabel("Was £200, then £150, now £100")
 }
 
-fun fixPriceLabel(priceLabel:String):String {
-    val listOfPrices = priceLabel.split(", ")
+fun fixPriceLabel(priceLabelString:String):String {
 
 
-    class priceLabel(val prefix:String, val price:String){
-        constructor (original:String ):this("",original.removePrefix("Was ").removePrefix("then ").removePrefix("now "))
-        var value = price.removePrefix("£").toDouble()
-    }
+    class PriceLabel(original: String) {
+        val price:String
+        val value:Double
 
-    when {
-        (listOfPrices.size == 2) -> {
-            val firstPriceLabel = priceLabel(listOfPrices[0])
-            val secondPriceLabel = priceLabel(listOfPrices[1])
-            if (firstPriceLabel.value > secondPriceLabel.value) {
-                return priceLabel
-            } else {
-                return "now ${secondPriceLabel.price}"
-            }
+        init {
+            price  = (original.removePrefix("Was ").removePrefix("then ").removePrefix("now "))
+            value = price.removePrefix("£").toDouble()
         }
-        (listOfPrices.size == 3) -> {
-            val firstPriceLabel = priceLabel(listOfPrices[0])
-            val secondPriceLabel = priceLabel(listOfPrices[1])
-            val thirdPriceLabel = priceLabel(listOfPrices[2])
-            if (secondPriceLabel.value > thirdPriceLabel.value) {
-                if (secondPriceLabel.value < firstPriceLabel.value) {
-                    return priceLabel
-                } else {
-                    return "Was ${secondPriceLabel.price}, now ${thirdPriceLabel.price}"
-                }
-            } else {
-                return "Was ${firstPriceLabel.price}, now ${thirdPriceLabel.price}"
-            }
-        }
-        else -> {
-            return priceLabel
+        fun toString(index:Int, lastIndex:Int) = when(index) {
+            lastIndex -> "now $price"
+            0 -> "Was $price, "
+            else -> "then $price, "
         }
     }
+
+    val ListOfLabels = priceLabelString.split(", ").map{PriceLabel(it)}
+
+    fun removeInvalidLabels(listOfLabels:List<PriceLabel>, output:List<PriceLabel> = listOf()):List<PriceLabel> {
+        if (listOfLabels.isEmpty()) return output
+
+        val nextPriceLabel = listOfLabels.first()
+        val remainingPriceLabels = listOfLabels.drop(1)
+
+        return if (remainingPriceLabels.filter{ nextPriceLabel.value < it.value }.isEmpty()) {
+            removeInvalidLabels(listOfLabels.drop(1),output + nextPriceLabel)
+        } else {
+            removeInvalidLabels(remainingPriceLabels,output )
+        }
+    }
+    val validLabels = removeInvalidLabels(ListOfLabels)
+    return validLabels.mapIndexed { index, priceLabel -> priceLabel.toString(index, validLabels.size-1) }.joinToString("")
 }
