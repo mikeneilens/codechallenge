cd70d89d1dfe7ac512f866fe3856a8b669766f51
Mike Neilens <mike.neilens@gmail.com>
2019-03-31 17:37:07 +0100

    Extracted function to create string out of an array of prices

--- challenge2/src/main/kotlin/Main.kt
+++ challenge2/src/main/kotlin/Main.kt
@@ -2,44 +2,46 @@ fun main(args: Array<String>) {
     println(fixPriceLabel("Was £200, then £150, now £100"))
 }
 
+class PriceLabel(original: String) {
+    val price:String
+    val value:Double
+
+    init {
+        price  = (original.removePrefix("Was ").removePrefix("then ").removePrefix("now "))
+        value = price.removePrefix("£").toDouble()
+    }
+    fun addPrefix(lastPrice:PriceLabel, firstPrice:PriceLabel) = when {
+        this == lastPrice -> "now $price"
+        this == firstPrice -> "Was $price, "
+        else -> "then $price, "
+    }
+}
+
+fun List<PriceLabel>.allPricesAreLessThan(priceLabel:PriceLabel):Boolean {
+    return this.filter{ priceInList -> priceLabel.value <= priceInList.value }.isEmpty()
+}
+
+fun removeInvalidLabels(listOfLabels:List<PriceLabel>): List<PriceLabel> {
+
+    data class FoldData(val resultingLabels:List<PriceLabel>, val remainingLabels:List<PriceLabel>)
+
+    val resultOfFold:FoldData = listOfLabels.fold(FoldData(listOf(),listOfLabels)){ foldData, priceLabel ->
+        val remainingLabels = foldData.remainingLabels.drop(1)
+        val resultingLabels = foldData.resultingLabels
+        if (remainingLabels.allPricesAreLessThan(priceLabel)) FoldData(resultingLabels + priceLabel, remainingLabels)
+        else FoldData(resultingLabels, remainingLabels)
+    }
+
+    return resultOfFold.resultingLabels
+}
+
+fun List<PriceLabel>.toPriceLabelString():String {
+    return this.map { priceLabel -> priceLabel.addPrefix(this.last(), this.first()) }.joinToString("")
+}
+
 fun fixPriceLabel(priceLabelString:String):String {
 
-
-    class PriceLabel(original: String) {
-        val price:String
-        val value:Double
-
-        init {
-            price  = (original.removePrefix("Was ").removePrefix("then ").removePrefix("now "))
-            value = price.removePrefix("£").toDouble()
-        }
-        fun addPrefix(lastPrice:PriceLabel, firstPrice:PriceLabel) = when {
-            this == lastPrice -> "now $price"
-            this == firstPrice -> "Was $price, "
-            else -> "then $price, "
-        }
-    }
-
     val ListOfLabels = priceLabelString.split(", ").map{PriceLabel(it)}
-
-    fun List<PriceLabel>.allPricesAreLessThan(priceLabel:PriceLabel):Boolean {
-        return this.filter{ priceInList -> priceLabel.value <= priceInList.value }.isEmpty()
-    }
-
-    fun removeInvalidLabels(listOfLabels:List<PriceLabel>): List<PriceLabel> {
-
-        data class FoldData(val resultingLabels:List<PriceLabel>, val remainingLabels:List<PriceLabel>)
-
-        val resultOfFold:FoldData = listOfLabels.fold(FoldData(listOf(),listOfLabels)){ foldData, priceLabel ->
-            val remainingLabels = foldData.remainingLabels.drop(1)
-            val resultingLabels = foldData.resultingLabels
-            if (remainingLabels.allPricesAreLessThan(priceLabel)) FoldData(resultingLabels + priceLabel, remainingLabels)
-            else FoldData(resultingLabels, remainingLabels)
-        }
-
-        return resultOfFold.resultingLabels
-    }
-
     val validLabels = removeInvalidLabels(ListOfLabels)
-    return validLabels.map { priceLabel -> priceLabel.addPrefix(validLabels.last(), validLabels.first()) }.joinToString("")
+    return validLabels.toPriceLabelString()
 }
