451e5fde3936beb386e1e23e8b17fca0bdf507e4
Mike Neilens <mike.neilens@gmail.com>
2019-03-31 16:33:32 +0100

    replaced recursive function with fold

--- challenge2/src/main/kotlin/Main.kt
+++ challenge2/src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    fixPriceLabel("Was £200, then £150, now £100")
+    println(fixPriceLabel("Was £200, then £150, now £100"))
 }
 
 fun fixPriceLabel(priceLabelString:String):String {
@@ -13,7 +13,7 @@ fun fixPriceLabel(priceLabelString:String):String {
             price  = (original.removePrefix("Was ").removePrefix("then ").removePrefix("now "))
             value = price.removePrefix("£").toDouble()
         }
-        fun toString(index:Int, lastIndex:Int) = when(index) {
+        fun addPrefix(index:Int, lastIndex:Int) = when(index) {
             lastIndex -> "now $price"
             0 -> "Was $price, "
             else -> "then $price, "
@@ -22,18 +22,20 @@ fun fixPriceLabel(priceLabelString:String):String {
 
     val ListOfLabels = priceLabelString.split(", ").map{PriceLabel(it)}
 
-    fun removeInvalidLabels(listOfLabels:List<PriceLabel>, output:List<PriceLabel> = listOf()):List<PriceLabel> {
-        if (listOfLabels.isEmpty()) return output
+    fun removeInvalidLabels(listOfLabels:List<PriceLabel>): List<PriceLabel> {
 
-        val nextPriceLabel = listOfLabels.first()
-        val remainingPriceLabels = listOfLabels.drop(1)
+        data class FoldData(val resultingLabels:List<PriceLabel>, val remainingLabels:List<PriceLabel>)
 
-        return if (remainingPriceLabels.filter{ nextPriceLabel.value <= it.value }.isEmpty()) {
-            removeInvalidLabels(listOfLabels.drop(1),output + nextPriceLabel)
-        } else {
-            removeInvalidLabels(remainingPriceLabels,output )
+        val resultOfFold:FoldData = listOfLabels.fold(FoldData(listOf(),listOfLabels)){ foldData, priceLabel ->
+            val remainingLabels = foldData.remainingLabels.drop(1)
+            val resultingLabels = foldData.resultingLabels
+            if (remainingLabels.filter{ remainingPrice -> priceLabel.value <= remainingPrice.value }.isEmpty()) FoldData(resultingLabels + priceLabel, remainingLabels)
+             else FoldData(resultingLabels, remainingLabels)
         }
+
+        return resultOfFold.resultingLabels
     }
+
     val validLabels = removeInvalidLabels(ListOfLabels)
-    return validLabels.mapIndexed { index, priceLabel -> priceLabel.toString(index, validLabels.size-1) }.joinToString("")
+    return validLabels.mapIndexed { index, priceLabel -> priceLabel.addPrefix(index, validLabels.size-1) }.joinToString("")
 }
