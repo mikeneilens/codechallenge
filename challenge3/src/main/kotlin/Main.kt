87c4d26f69d8ac74511828ff6397478d946fa201
Mike Neilens <mike.neilens@gmail.com>
2019-04-12 23:08:39 +0100

    Added logic to move person on map

--- challenge3/src/main/kotlin/Main.kt
+++ challenge3/src/main/kotlin/Main.kt
@@ -27,27 +27,89 @@ data class Position(val row:Int, val column:Int) {
     override fun equals(other: Any?): Boolean {
         return other is Position && this.column == other.column && this.row == other.row
     }
+    infix operator fun plus(other:Position):Position {
+        return Position(this.row + other.row, this.column + other.column)
+    }
 }
 
-fun Map<Position, MapTile>.toString(row:Int):String {
-    val listOfPositionMapTile = this.filter { mapTile -> mapTile.key.row == row }.toSortedMap(compareBy { position ->  position.column }).toList()
-    return listOfPositionMapTile.fold("") { acc, pair -> acc + pair.second.text}
-}
-
-fun List<String>.toMap():Map<Position,MapTile> {
+fun List<String>.toMap():MutableMap<Position,MapTile> {
     val mutableMap= mutableMapOf<Position,MapTile>()
     this.forEachIndexed{ row, string ->
-        mutableMap.fromString(row, string)
+        mutableMap.fromStringForRow(row, string)
     }
     return mutableMap
 }
 
-fun MutableMap<Position, MapTile>.fromString(row:Int, string:String) {
+fun MutableMap<Position, MapTile>.fromStringForRow(row:Int, string:String) {
     string.forEachIndexed{ column, character ->
         this[Position(row, column)] = MapTile.fromChar(character)
     }
 }
 
 fun Map<Position, MapTile>.toListOfString():List<String> {
-    return this.keys.map{it.row }.distinct().sorted().map{row -> this.toString(row)}
+    return this.keys.map{it.row }.distinct().sorted().map{row -> this.toStringForRow(row)}
 }
+
+fun Map<Position, MapTile>.toStringForRow(row:Int):String {
+    val listOfPositionMapTile = this.filter { mapTile -> mapTile.key.row == row }.toSortedMap(compareBy { position ->  position.column }).toList()
+    return listOfPositionMapTile.fold("") { acc, pair -> acc + pair.second.text}
+}
+
+enum class Direction(val move:Position) {
+    L (Position(0,-1)),
+    R (Position(0,1)),
+    U (Position(-1,0)),
+    D (Position(1,0))
+}
+
+fun processSokobanMove(listOfString:List<String>, direction:String):List<String> {
+    val gameMap = listOfString.toMap()
+    val directionToMove = Direction.valueOf(direction)
+    gameMap.movePerson(directionToMove)
+    return gameMap.toListOfString()
+}
+
+fun MutableMap<Position, MapTile>.movePerson(direction: Direction) {
+    val positionOfPerson = this.positionOfPerson()
+    val positionToMoveTo = positionOfPerson + direction.move
+
+    when {
+        this.canMoveOnto(positionToMoveTo) -> movePerson(positionToMoveTo, positionOfPerson)
+        this.containsBlock(positionToMoveTo) -> tryAndMoveBlock(positionToMoveTo, direction, positionOfPerson)
+    }
+}
+
+fun Map<Position, MapTile>.positionOfPerson():Position {
+    return this.filterValues{it == MapTile.Person || it == MapTile.PersonOnStorage  }.keys.first()
+}
+
+fun Map<Position, MapTile>.canMoveOnto(position:Position):Boolean {
+    return this[position] == MapTile.Empty || this[position] == MapTile.Storage
+}
+
+fun Map<Position, MapTile>.containsBlock(position:Position):Boolean {
+    return this[position] == MapTile.Block || this[position] == MapTile.BlockOnStorage
+}
+
+private fun MutableMap<Position, MapTile>.tryAndMoveBlock(positionOfBlock:Position, direction: Direction, positionOfPerson: Position) {
+    val positionAdjacentToBlock = positionOfBlock + direction.move
+    if (this.canMoveOnto(positionAdjacentToBlock)) {
+        moveBlock(positionAdjacentToBlock, positionOfBlock)
+        movePerson(positionOfBlock, positionOfPerson)
+    }
+}
+
+fun MutableMap<Position, MapTile>.movePerson(positionToMoveTo: Position, positionToMoveFrom: Position) {
+    val mapTileForOldPosition = if (this[positionToMoveFrom] == MapTile.Person) MapTile.Empty else MapTile.Storage
+    val mapTileForNewPosition = if (this[positionToMoveTo] == MapTile.Empty) MapTile.Person else MapTile.PersonOnStorage
+    this[positionToMoveFrom] = mapTileForOldPosition
+    this[positionToMoveTo] = mapTileForNewPosition
+}
+
+fun MutableMap<Position, MapTile>.moveBlock(positionToMoveTo: Position, positionToMoveFrom: Position) {
+    val mapTileForOldPosition = if (this[positionToMoveFrom] == MapTile.Block) MapTile.Empty else MapTile.Storage
+    val mapTileForNewPosition = if (this[positionToMoveTo] == MapTile.Empty) MapTile.Block else MapTile.BlockOnStorage
+    this[positionToMoveFrom] = mapTileForOldPosition
+    this[positionToMoveTo] = mapTileForNewPosition
+}
+
--- challenge3/src/test/kotlin/MainTest.kt
+++ challenge3/src/test/kotlin/MainTest.kt
@@ -37,7 +37,7 @@ class MainTest {
         map1[Position(1,4)] = MapTile.Wall
         map1[Position(1,5)] = MapTile.Storage
         map1[Position(1,6)] = MapTile.Empty
-        TestCase.assertEquals("pPbB#* ",map1.toString(1))
+        TestCase.assertEquals("pPbB#* ",map1.toStringForRow(1))
         val map2 = mutableMapOf<Position, MapTile>()
         map2[Position(1,1)] = MapTile.Person
         map2[Position(1,3)] = MapTile.PersonOnStorage
@@ -46,7 +46,7 @@ class MainTest {
         map2[Position(1,6)] = MapTile.Wall
         map2[Position(1,5)] = MapTile.Storage
         map2[Position(1,0)] = MapTile.Empty
-        TestCase.assertEquals(" pbPB*#",map2.toString(1))
+        TestCase.assertEquals(" pbPB*#",map2.toStringForRow(1))
     }
     @Test
     fun `Test List of String toMap`() {
@@ -82,5 +82,390 @@ class MainTest {
         map[Position(2,5)] = MapTile.Storage
         map[Position(2,6)] = MapTile.Wall
         TestCase.assertEquals(listOf("pPbB#* "," pbPB*#"),map.toListOfString())
+    }
+    @Test
+    fun `Test position of person in Map of Positions and Map Tiles` () {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Block
+        map[Position(1,2)] = MapTile.Block
+        map[Position(1,3)] = MapTile.BlockOnStorage
+        map[Position(1,4)] = MapTile.Wall
+        map[Position(1,5)] = MapTile.Storage
+        map[Position(1,6)] = MapTile.Empty
+        map[Position(2,0)] = MapTile.Empty
+        map[Position(2,1)] = MapTile.Person
+        map[Position(2,2)] = MapTile.Block
+        map[Position(2,3)] = MapTile.Block
+        map[Position(2,4)] = MapTile.BlockOnStorage
+        map[Position(2,5)] = MapTile.Storage
+        map[Position(2,6)] = MapTile.Wall
+        TestCase.assertEquals(Position(2,1),map.positionOfPerson())
+        val map2 = mutableMapOf<Position, MapTile>()
+        map2[Position(1,0)] = MapTile.Wall
+        map2[Position(1,1)] = MapTile.Block
+        map2[Position(1,2)] = MapTile.Block
+        map2[Position(1,3)] = MapTile.BlockOnStorage
+        map2[Position(1,4)] = MapTile.PersonOnStorage
+        map2[Position(1,5)] = MapTile.Storage
+        map2[Position(1,6)] = MapTile.Empty
+        map2[Position(2,0)] = MapTile.Empty
+        map2[Position(2,1)] = MapTile.Empty
+        map2[Position(2,2)] = MapTile.Block
+        map2[Position(2,3)] = MapTile.Block
+        map2[Position(2,4)] = MapTile.BlockOnStorage
+        map2[Position(2,5)] = MapTile.Storage
+        map2[Position(2,6)] = MapTile.Wall
+        TestCase.assertEquals(Position(1,4),map2.positionOfPerson())
+    }
+
+    @Test
+    fun `Test adding positions together`() {
+        TestCase.assertEquals(Position(2,3),Position(1,1) + Position(1,2))
+        TestCase.assertEquals(Position(0,-1),Position(1,1) + Position(-1,-2))
+        TestCase.assertEquals(Position(4,6),Position(1,2) + Position(3,4))
+    }
+
+    @Test
+    fun `Test canMoveOnto a square if it is empty or is storage and cant move onto any other squares`(){
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Person
+        TestCase.assertEquals(false,map.canMoveOnto(Position(1,0)) )
+
+        map[Position(1,1)] = MapTile.PersonOnStorage
+        TestCase.assertEquals(false,map.canMoveOnto(Position(1,1)) )
+
+        map[Position(1,2)] = MapTile.Block
+        TestCase.assertEquals(false,map.canMoveOnto(Position(1,2)) )
+
+        map[Position(1,3)] = MapTile.BlockOnStorage
+        TestCase.assertEquals(false,map.canMoveOnto(Position(1,3)))
+
+        map[Position(1,4)] = MapTile.Wall
+        TestCase.assertEquals(false,map.canMoveOnto(Position(1,4)) )
+
+        map[Position(1,5)] = MapTile.Storage
+        TestCase.assertEquals(true,map.canMoveOnto(Position(1,5)) )
+
+        map[Position(1,6)] = MapTile.Empty
+        TestCase.assertEquals(true,map.canMoveOnto(Position(1,6)) )
+
+    }
+    @Test
+    fun `Test if position on map contains a block`(){
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Person
+        TestCase.assertEquals(false,map.containsBlock(Position(1,0)) )
+
+        map[Position(1,2)] = MapTile.Block
+        TestCase.assertEquals(true,map.containsBlock(Position(1,2)) )
+
+        map[Position(1,3)] = MapTile.BlockOnStorage
+        TestCase.assertEquals(true,map.containsBlock(Position(1,3)))
+
+        map[Position(1,4)] = MapTile.Wall
+        TestCase.assertEquals(false,map.containsBlock(Position(1,4)) )
+
+    }
+    @Test
+    fun `Test moving a person to an empty square on the map`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Empty
+        map[Position(1,2)] = MapTile.Person
+        map[Position(1,3)] = MapTile.Empty
+        map[Position(1,4)] = MapTile.Wall
+
+        val positionToMoveFrom = Position(1, 2)
+        val postionToMoveTo = Position(1,3)
+        map.movePerson(postionToMoveTo,positionToMoveFrom)
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.Person, map[Position(1,3)])
+    }
+
+    @Test
+    fun `Test moving a person to an storage square on the map`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Empty
+        map[Position(1,2)] = MapTile.Person
+        map[Position(1,3)] = MapTile.Storage
+        map[Position(1,4)] = MapTile.Wall
+
+        val positionToMoveFrom = Position(1, 2)
+        val postionToMoveTo = Position(1,3)
+        map.movePerson(postionToMoveTo,positionToMoveFrom)
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.PersonOnStorage, map[Position(1,3)])
+    }
+
+    @Test
+    fun `Test moving a person frorm a storage square on the map`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Empty
+        map[Position(1,2)] = MapTile.PersonOnStorage
+        map[Position(1,3)] = MapTile.Empty
+        map[Position(1,4)] = MapTile.Wall
+
+        val positionToMoveFrom = Position(1, 2)
+        val postionToMoveTo = Position(1,3)
+        map.movePerson(postionToMoveTo,positionToMoveFrom)
+        TestCase.assertEquals(MapTile.Storage, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.Person, map[Position(1,3)])
+    }
+
+    @Test
+    fun `Test moving a block to an empty square on the map`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Empty
+        map[Position(1,2)] = MapTile.Block
+        map[Position(1,3)] = MapTile.Empty
+        map[Position(1,4)] = MapTile.Wall
+
+        val positionToMoveFrom = Position(1, 2)
+        val postionToMoveTo = Position(1,3)
+        map.moveBlock(postionToMoveTo,positionToMoveFrom)
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.Block, map[Position(1,3)])
+    }
+
+    @Test
+    fun `Test moving a block to an storage square on the map`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Empty
+        map[Position(1,2)] = MapTile.Block
+        map[Position(1,3)] = MapTile.Storage
+        map[Position(1,4)] = MapTile.Wall
+
+        val positionToMoveFrom = Position(1, 2)
+        val postionToMoveTo = Position(1,3)
+        map.moveBlock(postionToMoveTo,positionToMoveFrom)
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.BlockOnStorage, map[Position(1,3)])
+    }
+
+    @Test
+    fun `Test moving a block from a storage square on the map`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Empty
+        map[Position(1,2)] = MapTile.BlockOnStorage
+        map[Position(1,3)] = MapTile.Empty
+        map[Position(1,4)] = MapTile.Wall
+
+        val positionToMoveFrom = Position(1, 2)
+        val postionToMoveTo = Position(1,3)
+        map.moveBlock(postionToMoveTo,positionToMoveFrom)
+        TestCase.assertEquals(MapTile.Storage, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.Block, map[Position(1,3)])
+    }
+
+    @Test
+    fun `Test moving a person left`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Empty
+        map[Position(1,2)] = MapTile.Person
+        map[Position(1,3)] = MapTile.Empty
+        map[Position(1,4)] = MapTile.Wall
+
+        map.movePerson(Direction.L)
+        TestCase.assertEquals(MapTile.Wall, map[Position(1,0)])
+        TestCase.assertEquals(MapTile.Person, map[Position(1,1)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,3)])
+        TestCase.assertEquals(MapTile.Wall, map[Position(1,4)])
+    }
+    @Test
+    fun `Test moving a person right`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Empty
+        map[Position(1,2)] = MapTile.Person
+        map[Position(1,3)] = MapTile.Empty
+        map[Position(1,4)] = MapTile.Wall
+
+        map.movePerson(Direction.R)
+        TestCase.assertEquals(MapTile.Wall, map[Position(1,0)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,1)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.Person, map[Position(1,3)])
+        TestCase.assertEquals(MapTile.Wall, map[Position(1,4)])
+    }
+
+    @Test
+    fun `Test moving a person up`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(0,0)] = MapTile.Wall
+        map[Position(0,1)] = MapTile.Empty
+        map[Position(0,2)] = MapTile.Storage
+        map[Position(0,3)] = MapTile.Empty
+        map[Position(0,4)] = MapTile.Wall
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Empty
+        map[Position(1,2)] = MapTile.Person
+        map[Position(1,3)] = MapTile.Empty
+        map[Position(1,4)] = MapTile.Wall
+
+        map.movePerson(Direction.U)
+        TestCase.assertEquals(MapTile.Wall, map[Position(0,0)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(0,1)])
+        TestCase.assertEquals(MapTile.PersonOnStorage, map[Position(0,2)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(0,3)])
+        TestCase.assertEquals(MapTile.Wall, map[Position(0,4)])
+        TestCase.assertEquals(MapTile.Wall, map[Position(1,0)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,1)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,3)])
+        TestCase.assertEquals(MapTile.Wall, map[Position(1,4)])
+    }
+    @Test
+    fun `Test moving a person down`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(0,0)] = MapTile.Wall
+        map[Position(0,1)] = MapTile.Empty
+        map[Position(0,2)] = MapTile.Storage
+        map[Position(0,3)] = MapTile.Empty
+        map[Position(0,4)] = MapTile.Wall
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Empty
+        map[Position(1,2)] = MapTile.Person
+        map[Position(1,3)] = MapTile.Empty
+        map[Position(1,4)] = MapTile.Wall
+        map[Position(2,0)] = MapTile.Wall
+        map[Position(2,1)] = MapTile.Empty
+        map[Position(2,2)] = MapTile.Empty
+        map[Position(2,3)] = MapTile.Empty
+        map[Position(2,4)] = MapTile.Wall
+        map.movePerson(Direction.D)
+        TestCase.assertEquals(MapTile.Wall, map[Position(0,0)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(0,1)])
+        TestCase.assertEquals(MapTile.Storage, map[Position(0,2)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(0,3)])
+        TestCase.assertEquals(MapTile.Wall, map[Position(0,4)])
+        TestCase.assertEquals(MapTile.Wall, map[Position(1,0)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,1)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,3)])
+        TestCase.assertEquals(MapTile.Wall, map[Position(1,4)])
+        TestCase.assertEquals(MapTile.Wall, map[Position(2,0)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(2,1)])
+        TestCase.assertEquals(MapTile.Person, map[Position(2,2)])
+        TestCase.assertEquals(MapTile.Empty, map[Position(2,3)])
+        TestCase.assertEquals(MapTile.Wall, map[Position(2,4)])
+    }
+
+    @Test
+    fun `Test moving a person into a block that can move`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Person
+        map[Position(1,2)] = MapTile.Block
+        map[Position(1,3)] = MapTile.Empty
+        map[Position(1,4)] = MapTile.Wall
+
+        map.movePerson(Direction.R)
+        TestCase.assertEquals(MapTile.Empty, map[Position(1,1)])
+        TestCase.assertEquals(MapTile.Person, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.Block, map[Position(1,3)])
+    }
+
+    @Test
+    fun `Test moving a person into a block that cannot move`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Wall
+        map[Position(1,1)] = MapTile.Person
+        map[Position(1,2)] = MapTile.Block
+        map[Position(1,3)] = MapTile.Block
+        map[Position(1,4)] = MapTile.Wall
+
+        map.movePerson(Direction.R)
+        TestCase.assertEquals(MapTile.Person, map[Position(1,1)])
+        TestCase.assertEquals(MapTile.Block, map[Position(1,2)])
+        TestCase.assertEquals(MapTile.Block, map[Position(1,3)])
+    }
+
+    @Test
+    fun `Test processSokabanMove with simple move right` () {
+        val inputList = listOf(
+            "#############",
+            "#p        * #",
+            "#     b  b  #",
+            "# *         #",
+            "#############")
+        val aftermoveRight = listOf(
+            "#############",
+            "# p       * #",
+            "#     b  b  #",
+            "# *         #",
+            "#############")
+        TestCase.assertEquals(aftermoveRight, processSokobanMove(inputList,"R"))
+    }
+    @Test
+    fun `Test processSokabanMove with simple move down` () {
+        val inputList = listOf(
+            "#############",
+            "#p        * #",
+            "#     b  b  #",
+            "# *         #",
+            "#############")
+        val aftermoveDown = listOf(
+            "#############",
+            "#         * #",
+            "#p    b  b  #",
+            "# *         #",
+            "#############")
+        TestCase.assertEquals(aftermoveDown, processSokobanMove(inputList,"D"))
+    }
+    @Test
+    fun `Test processSokabanMove move block` () {
+        val inputList = listOf(
+            "#############",
+            "#         * #",
+            "#     b  b  #",
+            "# *   p     #",
+            "#############")
+        val aftermoveUp = listOf(
+            "#############",
+            "#     b   * #",
+            "#     p  b  #",
+            "# *         #",
+            "#############")
+        TestCase.assertEquals(aftermoveUp, processSokobanMove(inputList,"U"))
+    }
+    @Test
+    fun `Test processSokabanMove move block onto storage` () {
+        val inputList = listOf(
+            "#############",
+            "#         * #",
+            "#    *bp b  #",
+            "# *         #",
+            "#############")
+        val aftermoveLeft = listOf(
+            "#############",
+            "#         * #",
+            "#    Bp  b  #",
+            "# *         #",
+            "#############")
+        TestCase.assertEquals(aftermoveLeft, processSokobanMove(inputList,"L"))
+    }
+    @Test
+    fun `Test processSokabanMove move block off storage` () {
+        val inputList = listOf(
+            "#############",
+            "#         * #",
+            "#    Bp  b  #",
+            "# *         #",
+            "#############")
+        val aftermoveLeft = listOf(
+            "#############",
+            "#         * #",
+            "#   bP   b  #",
+            "# *         #",
+            "#############")
+        TestCase.assertEquals(aftermoveLeft, processSokobanMove(inputList,"L"))
     }
 }
