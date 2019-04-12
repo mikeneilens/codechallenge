5a0b21ac63b4a8a63c58784734cfe13ff2c4f752
Mike Neilens <mike.neilens@gmail.com>
2019-04-12 21:06:23 +0100

    Added code to parse array of strings into map and vice versa

--- challenge3/src/main/kotlin/Main.kt
+++ challenge3/src/main/kotlin/Main.kt
@@ -16,5 +16,35 @@ enum class MapTile(val text:String) {
             values().firstOrNull { it.text == string }?.let{return it}
             return Empty
         }
+        fun fromChar(char:Char):MapTile {
+            return this.fromString(char.toString())
+        }
     }
+}
+data class Position(val row:Int, val column:Int) {
+    override fun hashCode(): Int = column * 10000 + row
+    override fun equals(other: Any?): Boolean {
+        return other is Position && this.column == other.column && this.row == other.row
+    }
+}
+
+fun MutableMap<Position, MapTile>.fromString(row:Int, string:String) {
+    string.forEachIndexed{ column, character ->
+        this[Position(row, column)] = MapTile.fromChar(character)
+    }
+}
+fun MutableMap<Position, MapTile>.toString(row:Int):String {
+    val listOfPositionMapTile = this.filter { mapTile -> mapTile.key.row == row }.toSortedMap(compareBy { position ->  position.column }).toList()
+    return listOfPositionMapTile.fold("") { acc, pair -> acc + pair.second.text}
+}
+
+fun List<String>.toMap():MutableMap<Position,MapTile> {
+    val map= mutableMapOf<Position,MapTile>()
+    this.forEachIndexed{ row, string ->
+        map.fromString(row, string)
+    }
+    return map
+}
+fun MutableMap<Position, MapTile>.toListOfString():List<String> {
+    return this.keys.toList().map{it.row }.distinct().sorted().map{row -> this.toString(row)}
 }
--- challenge3/src/test/kotlin/MainTest.kt
+++ challenge3/src/test/kotlin/MainTest.kt
@@ -13,4 +13,74 @@ class MainTest {
         TestCase.assertEquals(MapTile.Empty, MapTile.fromString(" "))
         TestCase.assertEquals(MapTile.Empty, MapTile.fromString("any old data"))
     }
+    @Test
+    fun `Test MutableMap fromString`() {
+        val map1 = mutableMapOf<Position, MapTile>()
+        map1.fromString(1,"p")
+        TestCase.assertEquals(map1[Position(1,0)],MapTile.Person)
+        map1.fromString(1,"# pb*PB")
+        TestCase.assertEquals(map1[Position(1,0)],MapTile.Wall)
+        TestCase.assertEquals(map1[Position(1,1)],MapTile.Empty)
+        TestCase.assertEquals(map1[Position(1,2)],MapTile.Person)
+        TestCase.assertEquals(map1[Position(1,3)],MapTile.Block)
+        TestCase.assertEquals(map1[Position(1,4)],MapTile.Storage)
+        TestCase.assertEquals(map1[Position(1,5)],MapTile.PersonOnStorage)
+        TestCase.assertEquals(map1[Position(1,6)],MapTile.BlockOnStorage)
+    }
+    @Test
+    fun `Test MutableMap toString`() {
+        val map1 = mutableMapOf<Position, MapTile>()
+        map1[Position(1,0)] = MapTile.Person
+        map1[Position(1,1)] = MapTile.PersonOnStorage
+        map1[Position(1,2)] = MapTile.Block
+        map1[Position(1,3)] = MapTile.BlockOnStorage
+        map1[Position(1,4)] = MapTile.Wall
+        map1[Position(1,5)] = MapTile.Storage
+        map1[Position(1,6)] = MapTile.Empty
+        TestCase.assertEquals("pPbB#* ",map1.toString(1))
+        val map2 = mutableMapOf<Position, MapTile>()
+        map2[Position(1,1)] = MapTile.Person
+        map2[Position(1,3)] = MapTile.PersonOnStorage
+        map2[Position(1,2)] = MapTile.Block
+        map2[Position(1,4)] = MapTile.BlockOnStorage
+        map2[Position(1,6)] = MapTile.Wall
+        map2[Position(1,5)] = MapTile.Storage
+        map2[Position(1,0)] = MapTile.Empty
+        TestCase.assertEquals(" pbPB*#",map2.toString(1))
+    }
+    @Test
+    fun `Test List of String toMap`() {
+        val listOfStrings = listOf<String>("# p #", "B*Ppb")
+        val map = listOfStrings.toMap()
+        TestCase.assertEquals(map[Position(0,0)],MapTile.Wall)
+        TestCase.assertEquals(map[Position(0,1)],MapTile.Empty)
+        TestCase.assertEquals(map[Position(0,2)],MapTile.Person)
+        TestCase.assertEquals(map[Position(0,3)],MapTile.Empty)
+        TestCase.assertEquals(map[Position(0,4)],MapTile.Wall)
+
+        TestCase.assertEquals(map[Position(1,0)],MapTile.BlockOnStorage)
+        TestCase.assertEquals(map[Position(1,1)],MapTile.Storage)
+        TestCase.assertEquals(map[Position(1,2)],MapTile.PersonOnStorage)
+        TestCase.assertEquals(map[Position(1,3)],MapTile.Person)
+        TestCase.assertEquals(map[Position(1,4)],MapTile.Block)
+    }
+    @Test
+    fun `Test Map to ListOfString`() {
+        val map = mutableMapOf<Position, MapTile>()
+        map[Position(1,0)] = MapTile.Person
+        map[Position(1,1)] = MapTile.PersonOnStorage
+        map[Position(1,2)] = MapTile.Block
+        map[Position(1,3)] = MapTile.BlockOnStorage
+        map[Position(1,4)] = MapTile.Wall
+        map[Position(1,5)] = MapTile.Storage
+        map[Position(1,6)] = MapTile.Empty
+        map[Position(2,0)] = MapTile.Empty
+        map[Position(2,1)] = MapTile.Person
+        map[Position(2,2)] = MapTile.Block
+        map[Position(2,3)] = MapTile.PersonOnStorage
+        map[Position(2,4)] = MapTile.BlockOnStorage
+        map[Position(2,5)] = MapTile.Storage
+        map[Position(2,6)] = MapTile.Wall
+        TestCase.assertEquals(listOf("pPbB#* "," pbPB*#"),map.toListOfString())
+    }
 }
