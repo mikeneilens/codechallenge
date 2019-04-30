44fddd26b6e22bf69f1f0aaef7b88c6d7ac1cd28
Mike Neilens <mike.neilens@gmail.com>
2019-04-30 20:54:50 +0100

    refactored to calc rank of each voucher as a single value instead of 3 separate values

--- challenge4/src/main/kotlin/Main.kt
+++ challenge4/src/main/kotlin/Main.kt
@@ -16,15 +16,10 @@ fun sortVouchers(vouchers: String): String {
     val voucherStatusGroup0 = if (listOfVouchers[0].contains("Activated") || listOfVouchers[0].contains("Available") ) 0 else 10000000
     val voucherStatusGroup1 = if (listOfVouchers[1].contains("Activated") || listOfVouchers[1].contains("Available") ) 0 else 10000000
 
-    val voucher0Rank = voucherStatusGroup0 + voucherDate0
+    val voucher0Rank = voucherStatusGroup0 + voucherDate0 + voucherstatusValue0
+    val voucher1Rank = voucherStatusGroup1 + voucherDate1 + voucherstatusValue1
 
-    return when {
-        voucherStatusGroup0 < voucherStatusGroup1  -> vouchers
-        voucherStatusGroup0 > voucherStatusGroup1  -> listOfVouchers[1] + ";" + listOfVouchers[0]
-        voucherDate0 < voucherDate1  -> vouchers
-        voucherDate0 > voucherDate1  -> listOfVouchers[1] + ";" + listOfVouchers[0]
-        voucherstatusValue0 < voucherstatusValue1 -> vouchers
-        voucherstatusValue0 > voucherstatusValue1 -> listOfVouchers[1] + ";" + listOfVouchers[0]
-        else -> vouchers
-    }
+
+    return if (voucher0Rank < voucher1Rank) vouchers else listOfVouchers[1] + ";" + listOfVouchers[0]
+
 }
