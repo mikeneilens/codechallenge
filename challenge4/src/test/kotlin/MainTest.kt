13f942554b77115f97a39df7a797626fb4049c5c
Mike Neilens <mike.neilens@gmail.com>
2019-04-30 18:49:50 +0100

    Startede code challenge4

---
+++ challenge4/src/test/kotlin/MainTest.kt
@@ -0,0 +1,20 @@
+import junit.framework.TestCase
+import org.junit.Test
+
+class MainTest {
+    @Test
+    fun `Single voucher returns the same single voucher`() {
+        TestCase.assertEquals("190112:Activated:aaaa",sortVouchers("190112:Activated:aaaa"))
+    }
+
+    @Test
+    fun `Test two vouchers in the correct sequence return the same two vouchers in the correct sequence`() {
+        TestCase.assertEquals("190112:Activated:aaaa;190113:Activated:bbbb",sortVouchers("190112:Activated:aaaa;190113:Activated:bbbb"))
+    }
+    
+    @Test
+    fun `Test two vouchers in the incorrect date sequence return the same two vouchers in the correct date sequence`() {
+        TestCase.assertEquals("190112:Activated:aaaa;190113:Activated:bbbb",sortVouchers("190113:Activated:aaaa;190112:Activated:bbbb"))
+    }
+
+}
