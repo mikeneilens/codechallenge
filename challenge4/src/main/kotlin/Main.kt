231b945595257c847a9214e74bb53678fad534fa
Mike Neilens <mike.neilens@gmail.com>
2019-05-02 08:21:16 +0100

    Added the example from the exercise to my tests.

--- challenge4/src/main/kotlin/Main.kt
+++ challenge4/src/main/kotlin/Main.kt
@@ -1,21 +1,17 @@
-fun main(args: Array<String>) {
-    println("Hello world")
-}
-
 fun sortVouchers(vouchers: String): String {
     return vouchers
-        .split(";")
+        .split(",")
         .sortedBy { voucher -> voucher.rank() }
-        .joinToString(";")
+        .joinToString(",")
 }
 
 //rank is an 8 digit number used to rank vouchers
 fun String.rank():Int {
     val isCurrentReward = (this.contains("Activated") || this.contains("Available") )
 
-    val voucherStatusGroup = if (isCurrentReward)  0 else 10000000
-    val voucherDate = if (isCurrentReward) this.substring(0,6).toInt() * 10 else 9999999 - this.substring(0,6).toInt() * 10
-    val voucherstatusValue = if (this.contains("Activated") || this.contains("Expired")) 0 else 1
+    val voucherStatusGroupRanking = if (isCurrentReward)  0 else 10000000
+    val voucherDateRanking = if (isCurrentReward) this.substring(0,6).toInt() * 10 else 9999999 - this.substring(0,6).toInt() * 10
+    val voucherStatusRanking = if (this.contains("Activated") || this.contains("Redeemed")) 0 else 1
 
-    return voucherStatusGroup + voucherDate + voucherstatusValue
+    return voucherStatusGroupRanking + voucherDateRanking + voucherStatusRanking
 }
--- challenge4/src/test/kotlin/MainTest.kt
+++ challenge4/src/test/kotlin/MainTest.kt
@@ -9,41 +9,47 @@ class MainTest {
 
     @Test
     fun `Test two vouchers in the correct sequence return the same two vouchers in the correct sequence`() {
-        TestCase.assertEquals("190112:Activated:aaaa;190113:Activated:bbbb",sortVouchers("190112:Activated:aaaa;190113:Activated:bbbb"))
+        TestCase.assertEquals("190112:Activated:aaaa,190113:Activated:bbbb",sortVouchers("190112:Activated:aaaa,190113:Activated:bbbb"))
     }
 
     @Test
     fun `Test two vouchers in the incorrect date sequence return the same two vouchers in the correct date sequence`() {
-        TestCase.assertEquals("190112:Activated:bbbb;190113:Activated:aaaa",sortVouchers("190113:Activated:aaaa;190112:Activated:bbbb"))
+        TestCase.assertEquals("190112:Activated:bbbb,190113:Activated:aaaa",sortVouchers("190113:Activated:aaaa,190112:Activated:bbbb"))
     }
 
     @Test
     fun `Test two Expired or Redeemed vouchers in the incorrect date sequence return the same two vouchers in the correct date sequence`() {
-        TestCase.assertEquals("190113:Redeemed:bbbb;190112:Redeemed:aaaa",sortVouchers("190112:Redeemed:aaaa;190113:Redeemed:bbbb"))
+        TestCase.assertEquals("190113:Redeemed:bbbb,190112:Redeemed:aaaa",sortVouchers("190112:Redeemed:aaaa,190113:Redeemed:bbbb"))
     }
 
     @Test
     fun `Test two current vouchers in the incorrect status sequence return the same two vouchers in the correct status sequence`() {
-        TestCase.assertEquals("190112:Activated:bbbb;190112:Available:aaaa",sortVouchers("190112:Available:aaaa;190112:Activated:bbbb"))
+        TestCase.assertEquals("190112:Activated:bbbb,190112:Available:aaaa",sortVouchers("190112:Available:aaaa,190112:Activated:bbbb"))
     }
 
     @Test
     fun `Test two redeemed or expired vouchers in the incorrect status sequence return the same two vouchers in the correct status sequence`() {
-        TestCase.assertEquals("190112:Expired:bbbb;190112:Redeemed:aaaa",sortVouchers("190112:Redeemed:aaaa;190112:Expired:bbbb"))
+        TestCase.assertEquals("190112:Redeemed:bbbb,190112:Expired:aaaa",sortVouchers("190112:Expired:aaaa,190112:Redeemed:bbbb"))
     }
 
     @Test
     fun `Test two vouchers in the incorrect status group sequence return the same two vouchers in the correct status group sequence`() {
-        TestCase.assertEquals("190112:Available:bbbb;190112:Expired:aaaa",sortVouchers("190112:Expired:aaaa;190112:Available:bbbb"))
+        TestCase.assertEquals("190112:Available:bbbb,190112:Expired:aaaa",sortVouchers("190112:Expired:aaaa,190112:Available:bbbb"))
     }
 
     @Test
     fun `Test three vouchers in the incorrect date sequence return the same three vouchers in the correct date sequence`() {
-        TestCase.assertEquals("190112:Activated:cccc;190113:Activated:bbbb;190114:Activated:aaaa",sortVouchers("190114:Activated:aaaa;190113:Activated:bbbb;190112:Activated:cccc"))
+        TestCase.assertEquals("190112:Activated:cccc,190113:Activated:bbbb,190114:Activated:aaaa",sortVouchers("190114:Activated:aaaa,190113:Activated:bbbb,190112:Activated:cccc"))
     }
 
     @Test
     fun `Test empty string`() {
         TestCase.assertEquals("",sortVouchers(""))
+    }
+
+    @Test
+    fun `Test exmample from the exercise`() {
+        TestCase.assertEquals("190111:Activated:ffff,190111:Available:cccc,190112:Activated:bbbb,190112:Available:aaaa,190110:Redeemed:dddd,190110:Expired:eeee",
+            sortVouchers("190112:Available:aaaa,190112:Activated:bbbb,190111:Available:cccc,190110:Redeemed:dddd,190110:Expired:eeee,190111:Activated:ffff"))
     }
 }
