36b7e34c2beb8a174ca4853e2734577c04fc7890
Mike Neilens <mike.neilens@gmail.com>
2019-06-21 21:18:31 +0100

    Added function to create a function

--- challenge7/src/main/kotlin/Main.kt
+++ challenge7/src/main/kotlin/Main.kt
@@ -1,6 +1,19 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 fun main(args: Array<String>) {
 
 }
 
-fun numberIsEvenAndLessThan(something:Int, aNumber:Int) = aNumber % 2 == 0 && aNumber < something
+val numberIsEvenAndLessThanSomething = fun (something:Int, aNumber:Int)  = (aNumber % 2 == 0 && aNumber < something)
+
+val numberIsEvenAndLessThan = fun (something:Int):(Int) -> Boolean {
+        return {aNumber:Int -> numberIsEvenAndLessThanSomething(something, aNumber)  }
+    }
+
+typealias functionThatTakesTwoParms = (Int, Int) -> Boolean
+typealias functionThatTakesOneParm = (Int) -> Boolean
+
+fun functionThatTakesTwoParms.curried(something: Int):functionThatTakesOneParm {
+    return {aNumber:Int -> this(something,aNumber) }
+}
 
--- challenge7/src/test/kotlin/MainTest.kt
+++ challenge7/src/test/kotlin/MainTest.kt
@@ -3,10 +3,32 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun `Test the function isEvenAndLessThan` () {
-        assertEquals(true, numberIsEvenAndLessThan(5,2))
-        assertEquals(false, numberIsEvenAndLessThan(5,3))
-        assertEquals(false, numberIsEvenAndLessThan(5,6))
+    fun `Test the function  that takes two parameters called numberIsEvenAndLessThanSomething` () {
+        assertEquals(true, numberIsEvenAndLessThanSomething(5,2))
+        assertEquals(false, numberIsEvenAndLessThanSomething(5,3))
+        assertEquals(false, numberIsEvenAndLessThanSomething(5,6))
+    }
+
+    @Test
+    fun `test the function called isEvenAndLessThan that returns a function that accepts one parameter` () {
+        val numberIsEvenAndLessThan5 = numberIsEvenAndLessThan(5)
+
+        assertEquals(true, numberIsEvenAndLessThan5(2))
+        assertEquals(false, numberIsEvenAndLessThan5(3))
+        assertEquals(false, numberIsEvenAndLessThan5(6))
+    }
+
+    @Test
+    fun `Test the function called isEvenAndLessThan with a filter function` () {
+        assertEquals(listOf(2,4), listOf(1,2,3,4,5,6,7,8,9,10).filter (numberIsEvenAndLessThan(5)))
+    }
+
+    @Test
+    fun `test the curried function to make sure it produces a function the same as isEvenAndLessThan does` () {
+        val numberIsEvenAndLessThan5 = numberIsEvenAndLessThanSomething.curried(5)
+        assertEquals(true, numberIsEvenAndLessThan5(2))
+        assertEquals(false, numberIsEvenAndLessThan5(3))
+        assertEquals(false, numberIsEvenAndLessThan5(6))
     }
 }
 
