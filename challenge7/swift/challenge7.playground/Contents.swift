94283db4953737e26cbbf28aa614e3927c2ffcc7
Mike Neilens <mike.neilens@gmail.com>
2019-06-22 21:11:02 +0100

    Added function with two parms to swift

--- challenge7/swift/challenge7.playground/Contents.swift
+++ challenge7/swift/challenge7.playground/Contents.swift
@@ -1,1 +1,18 @@
+import XCTest
 var str = "Hello, playground"
+func numberIsEvenAndLessThanSomething(_ something:Int, _ aNumber:Int) ->Bool  {
+    return aNumber % 2 == 0 && aNumber < something
+}
+
+
+class Challenge7Tests: XCTestCase {
+    func test_numberIsEvenAndLessThanSomething() {
+        XCTAssertEqual(true, numberIsEvenAndLessThanSomething(5,2))
+        XCTAssertEqual(false, numberIsEvenAndLessThanSomething(5,3))
+        XCTAssertEqual(false, numberIsEvenAndLessThanSomething(5,6))
+    }
+}
+
+Challenge7Tests.defaultTestSuite.run()
+
+
