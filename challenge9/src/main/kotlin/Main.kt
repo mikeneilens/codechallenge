00f17370e0589c56704bd190d0b02c476d5be2bd
Mike Neilens <mike.neilens@gmail.com>
2019-07-25 20:45:53 +0100

    Created an empty GameLedger

--- challenge9/src/main/kotlin/Main.kt
+++ challenge9/src/main/kotlin/Main.kt
@@ -1,34 +1,3 @@
-interface Location
-
-interface Purchaseable:Location {
-    val purchasePrice:GBP
-}
-interface Buildable:Purchaseable {
-    val miniStore:DevelopmentType.BuildCostAndRent
-    val supermarket:DevelopmentType.BuildCostAndRent
-    val megastore:DevelopmentType.BuildCostAndRent
-}
-
-sealed class DevelopmentType{
-    class RentOnly(val rent:GBP)
-    class BuildCostAndRent(val buildingCost:GBP, val rent:GBP)
-}
-
-class GBP(_value:Int) {
-    val value:Int
-
-    init {
-        if (_value > 0 ) value = _value else value = - _value
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return other is GBP && other.value == value
-    }
-
-    override fun toString(): String {
-        return "£$value"
-    }
-}
 
 class Main {
 }
--- challenge9/src/test/kotlin/MainTest.kt
+++ challenge9/src/test/kotlin/MainTest.kt
@@ -3,4 +3,9 @@ import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class MainTest {
+    @Test
+    fun `Create an Empty GameLedger`() {
+        val gameLedger = GameLedger
+        assertTrue(gameLedger is GameLedger)
+    }
 }
