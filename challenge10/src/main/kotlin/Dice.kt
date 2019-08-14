d889901ba1f9b9bbabae441f3caebc24e3561935
mikeneilens <mike.neilens@gmail.com>
2019-08-14 19:26:02 +0100

    Update Dice.kt

--- challenge10/src/main/kotlin/Dice.kt
+++ challenge10/src/main/kotlin/Dice.kt
@@ -1,14 +1,12 @@
 import kotlin.random.Random
 
 class Dice {
-    private val diceOne:Int
-    private val diceTwo:Int
+    private val diceOne:Int = Random.nextInt(1, 7)
+    private val diceTwo:Int = Random.nextInt(1, 7)
 
     val totalValue:Int
 
     init {
-        diceOne = Random.nextInt(1, 7)
-        diceTwo = Random.nextInt(1, 7)
         totalValue = diceOne + diceTwo
     }
 
