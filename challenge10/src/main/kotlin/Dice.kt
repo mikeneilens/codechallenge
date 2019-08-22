6810f7987aedbb09a879bf872bc7dd65447b00f0
Mike Neilens <mike.neilens@gmail.com>
2019-08-22 19:25:22 +0100

    added jsonmapper

--- challenge10/src/main/kotlin/Dice.kt
+++ challenge10/src/main/kotlin/Dice.kt
@@ -1,8 +1,8 @@
 import kotlin.random.Random
 
-class Dice {
-    private val diceOne:Int = Random.nextInt(1, 7)
-    private val diceTwo:Int = Random.nextInt(1, 7)
+class Dice(_numberProvider:ProvidesNumberBetweenOneAndSix = randomDice) {
+    private val diceOne:Int = _numberProvider.numberBetweenOneAndSix // Random.nextInt(1, 7)
+    private val diceTwo:Int = _numberProvider.numberBetweenOneAndSix //Random.nextInt(1, 7)
 
     val totalValue:Int
 
@@ -13,4 +13,13 @@ class Dice {
     override fun toString(): String {
         return "You threw a $diceOne and a $diceTwo"
     }
+}
+
+interface ProvidesNumberBetweenOneAndSix {
+    val numberBetweenOneAndSix:Int
+}
+
+object randomDice:ProvidesNumberBetweenOneAndSix {
+    override val numberBetweenOneAndSix: Int
+        get() = Random.nextInt(1, 7)
 }
