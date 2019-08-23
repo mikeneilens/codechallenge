b402c82c441141010ec86c180827662ca9efba90
Mike Neilens <mike.neilens@gmail.com>
2019-08-23 11:46:45 +0100

    added function and tests to parse the json

--- challenge11/src/main/kotlin/TestData.kt
+++ challenge11/src/main/kotlin/TestData.kt
@@ -28,3 +28,91 @@ val singlePubWithNoBeer = "{\"Pubs\":[{\n" +
         "  \"Branch\": \"WLD\",\n" +
         "  \"CreateTS\": \"2019-05-16 19:31:39\",\n" +
         "}]}"
+val manyPubs = "{\n" +
+        "  \"Pubs\": [\n" +
+        "    {\n" +
+        "      \"Name\": \"Phoenix\",\n" +
+        "      \"Address\": \"14 Palace Street Victoria London SW1E 5JA\",\n" +
+        "      \"Town\": \"London\",\n" +
+        "      \"RegularBeers\": [\n" +
+        "        \"Young#039;s Bitter\"\n" +
+        "      ],\n" +
+        "      \"GuestBeers\": [\n" +
+        "        \"Sharp#039;s Doom Bar\",\n" +
+        "        \"Twickenham --varies--\",\n" +
+        "        \"Young#039;s --varies--\"\n" +
+        "      ],\n" +
+        "      \"PubService\": \"https://pubcrawlapi.appspot.com/pub/?v=1&id=16185&branch=WLD&uId=mike&pubs=no&realAle=yes&memberDiscount=no&town=London\",\n" +
+        "      \"Id\": \"16185\",\n" +
+        "      \"Branch\": \"WLD\",\n" +
+        "      \"CreateTS\": \"2019-05-16 19:31:20\",\n" +
+        "    },\n" +
+        "    {\n" +
+        "      \"Name\": \"Windsor Castle\",\n" +
+        "      \"Address\": \"23 Francis Street Westminster London SW1P 1DN\",\n" +
+        "      \"Town\": \"London\",\n" +
+        "      \"RegularBeers\": [\n" +
+        "        \"Samuel Smith Old Brewery Bitter\"\n" +
+        "      ],\n" +
+        "      \"PubService\": \"https://pubcrawlapi.appspot.com/pub/?v=1&id=16187&branch=WLD&uId=mike&pubs=no&realAle=yes&memberDiscount=no&town=London\",\n" +
+        "      \"Id\": \"16187\",\n" +
+        "      \"Branch\": \"WLD\",\n" +
+        "      \"CreateTS\": \"2019-05-16 19:31:20\",\n" +
+        "    },\n" +
+        "    {\n" +
+        "      \"Name\": \"Sports Bar and Grill Victoria\",\n" +
+        "      \"Address\": \"Unit 59 1st floor Victoria Main Line Station Terminus Place Victoria London SW1V 1JU\",\n" +
+        "      \"Town\": \"London\",\n" +
+        "      \"RegularBeers\": [\n" +
+        "        \"Brakspear Bitter\",\n" +
+        "        \"Marston#039;s EPA\"\n" +
+        "      ],\n" +
+        "      \"PubService\": \"https://pubcrawlapi.appspot.com/pub/?v=1&id=16196&branch=WLD&uId=mike&pubs=no&realAle=yes&memberDiscount=no&town=London\",\n" +
+        "      \"Id\": \"16196\",\n" +
+        "      \"Branch\": \"WLD\",\n" +
+        "      \"CreateTS\": \"2019-05-16 19:31:20\",\n" +
+        "    },\n" +
+        "    {\n" +
+        "      \"Name\": \"Willow Walk\",\n" +
+        "      \"Address\": \"25 Wilton Road Victoria London SW1V 1LW\",\n" +
+        "      \"Town\": \"London\",\n" +
+        "      \"RegularBeers\": [\n" +
+        "        \"Fuller#039;s London Pride\",\n" +
+        "        \"Greene King Abbot\",\n" +
+        "        \"Greene King IPA\"\n" +
+        "      ],\n" +
+        "      \"PubService\": \"https://pubcrawlapi.appspot.com/pub/?v=1&id=15951&branch=WLD&uId=mike&pubs=no&realAle=yes&memberDiscount=no&town=London\",\n" +
+        "      \"Id\": \"15951\",\n" +
+        "      \"Branch\": \"WLD\",\n" +
+        "      \"CreateTS\": \"2019-05-16 19:31:21\",\n" +
+        "    },\n" +
+        "    {\n" +
+        "      \"Name\": \"Buckingham Arms\",\n" +
+        "      \"Address\": \"62 Petty France Westminster London SW1H 9EU\",\n" +
+        "      \"Town\": \"London\",\n" +
+        "      \"RegularBeers\": [\n" +
+        "        \"Young#039;s --seasonal--\",\n" +
+        "        \"Young#039;s Bitter\",\n" +
+        "        \"Young#039;s London Gold\",\n" +
+        "        \"Young#039;s Special\"\n" +
+        "      ],\n" +
+        "      \"PubService\": \"https://pubcrawlapi.appspot.com/pub/?v=1&id=15905&branch=WLD&uId=mike&pubs=no&realAle=yes&memberDiscount=no&town=London\",\n" +
+        "      \"Id\": \"15905\",\n" +
+        "      \"Branch\": \"WLD\",\n" +
+        "      \"CreateTS\": \"2019-05-16 19:31:21\",\n" +
+        "    },\n" +
+        "    {\n" +
+        "      \"Name\": \"Shakespeare\",\n" +
+        "      \"Address\": \"99 Buckingham Palace Road Victoria London SW1W 0RP\",\n" +
+        "      \"Town\": \"London\",\n" +
+        "      \"RegularBeers\": [\n" +
+        "        \"Greene King IPA\",\n" +
+        "        \"Greene King IPA Reserve\"\n" +
+        "      ],\n" +
+        "      \"PubService\": \"https://pubcrawlapi.appspot.com/pub/?v=1&id=16066&branch=WLD&uId=mike&pubs=no&realAle=yes&memberDiscount=no&town=London\",\n" +
+        "      \"Id\": \"16066\",\n" +
+        "      \"Branch\": \"WLD\",\n" +
+        "      \"CreateTS\": \"2019-05-16 19:31:21\",\n" +
+        "    }\n" +
+        "  ]\n" +
+        "}"
--- challenge11/src/test/kotlin/MainTest.kt
+++ challenge11/src/test/kotlin/MainTest.kt
@@ -23,4 +23,31 @@ class MainTest {
         assertTrue(listOfPubs[0].regularBeers.isEmpty())
         assertTrue(listOfPubs[0].guestBeers.isEmpty())
     }
+
+    @Test
+    fun `ParseJsonIntoPubs parses a json array containing one pub with regular beers and guest beers`() {
+        val listOfPubs = ParseJsonIntoPubs(singlePub)
+
+        assertEquals(2, listOfPubs[0].regularBeers.size)
+        assertEquals("Shepherd Neame Master Brew",listOfPubs[0].regularBeers[0])
+        assertEquals("Shepherd Neame Spitfire",listOfPubs[0].regularBeers[1])
+
+        assertEquals(3, listOfPubs[0].guestBeers.size)
+        assertEquals("Shepherd Neame --seasonal--", listOfPubs[0].guestBeers[0])
+        assertEquals("Shepherd Neame --varies--", listOfPubs[0].guestBeers[1])
+        assertEquals("Shepherd Neame Whitstable Bay Pale Ale", listOfPubs[0].guestBeers[2])
+    }
+    @Test
+    fun `ParseJsonIntoPubs parses a json array containing more than one pub`() {
+        val listOfPubs = ParseJsonIntoPubs(manyPubs)
+
+        assertEquals(6, listOfPubs.size )
+        assertEquals("Phoenix", listOfPubs[0].name)
+        assertEquals(1, listOfPubs[0].regularBeers.size)
+        assertEquals(3, listOfPubs[0].guestBeers.size)
+
+        assertEquals("Willow Walk", listOfPubs[3].name)
+        assertEquals(3, listOfPubs[3].regularBeers.size)
+        assertEquals(0, listOfPubs[3].guestBeers.size)
+    }
 }
