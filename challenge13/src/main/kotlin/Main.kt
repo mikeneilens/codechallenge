5069534be6dc346a2324d034d3014ddfcc7d2e86
Mike Neilens <mike.neilens@gmail.com>
2019-09-30 18:54:46 +0100

    added function to calculate distance between shops

--- challenge13/src/main/kotlin/Main.kt
+++ challenge13/src/main/kotlin/Main.kt
@@ -2,21 +2,28 @@ import org.gavaghan.geodesy.Ellipsoid
 import org.gavaghan.geodesy.GeodeticCalculator
 import org.gavaghan.geodesy.GlobalCoordinates
 
+typealias DistanceInMiles = Double
+
 class Main {
 }
-
-class GeoLocation(val lat:Double, val lng:Double)
-
-class shop(val name:String, val postcode:String, geoLocation: GeoLocation )
 
 val geoCalc = GeodeticCalculator()
 val reference = Ellipsoid.WGS84
 val METRES_IN_A_MILE = 1609.34
 
-fun GeoLocation.distanceTo(other:GeoLocation):Double {
-    val globalCoordinates1 = GlobalCoordinates(this.lat, this.lng)
-    val globalCoordinates2 = GlobalCoordinates(other.lat, other.lng)
-    val distanceInMetres = geoCalc.calculateGeodeticCurve(reference, globalCoordinates1, globalCoordinates2).ellipsoidalDistance
+class GeoLocation(val lat:Double, val lng:Double) {
+    fun distanceTo(other:GeoLocation):DistanceInMiles {
+        val globalCoordinates1 = GlobalCoordinates(this.lat, this.lng)
+        val globalCoordinates2 = GlobalCoordinates(other.lat, other.lng)
+        val distanceInMetres = geoCalc.calculateGeodeticCurve(reference, globalCoordinates1, globalCoordinates2).ellipsoidalDistance
 
-    return distanceInMetres/METRES_IN_A_MILE
+        return distanceInMetres/METRES_IN_A_MILE
+    }
 }
+
+class Shop(val name:String, val postcode:String, val geoLocation: GeoLocation ) {
+    fun distanceTo(otherShop:Shop): DistanceInMiles =  this.geoLocation.distanceTo(otherShop.geoLocation)
+}
+
+
+
--- challenge13/src/test/kotlin/MainTest.kt
+++ challenge13/src/test/kotlin/MainTest.kt
@@ -2,10 +2,16 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MainTest {
+
+    val peterJones = Shop("Peter Jones", "SW3", GeoLocation(51.492246,-0.159 ))
+    val oxfordStreet = Shop("Oxford Street", "W1", GeoLocation(51.524565, -0.112042))
+    val liverpool = Shop("Liverpool", "L1", GeoLocation(53.403799, -2.987648))
+
+
     @Test
-    fun `distance between Liverpool and London should be 173`() {
-        val londonLocation = GeoLocation(51.524565, -0.112042)
-        val liverpoolLocation = GeoLocation(53.403799, -2.987648)
+    fun `distance between Liverpool and London should be 177`() {
+        val londonLocation = oxfordStreet.geoLocation
+        val liverpoolLocation = liverpool.geoLocation
 
         val distanceBetweenLiverpoolAndLondon = londonLocation.distanceTo(liverpoolLocation).toInt()
 
@@ -13,12 +19,28 @@ class MainTest {
     }
 
     @Test
-    fun `distance between Peter Jones and Oxford Street shops should be`() {
-        val peterJonesLocation = GeoLocation(51.492246,-0.159 )
-        val oxfordStreetLocation = GeoLocation(51.524565, -0.112042)
+    fun `distance between Peter Jones location and Oxford Street location should be 3 miles`() {
+        val peterJonesLocation = peterJones.geoLocation
+        val oxfordStreetLocation = oxfordStreet.geoLocation
 
         val distanceBetweenPeterJonesAndOxfordStreet = peterJonesLocation.distanceTo(oxfordStreetLocation).toInt()
 
         assertEquals(3, distanceBetweenPeterJonesAndOxfordStreet)
+    }
+
+    @Test
+    fun `distance to same shop is zero `() {
+
+        val distanceToSamePlace = peterJones.distanceTo(peterJones)
+
+        assertEquals(0.0,distanceToSamePlace)
+    }
+
+    @Test
+    fun `distance between Peter Jones shop and Oxford Street shop is roughly 3 miles `() {
+
+        val distanceBeweenPeterJonesAndOxfordStree = peterJones.distanceTo(oxfordStreet).toInt()
+
+        assertEquals(3,distanceBeweenPeterJonesAndOxfordStree)
     }
 }
