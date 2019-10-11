4c6a6a6c3edead720deff1cef96581b3e02f5ae1
Mike Neilens <mike.neilens@gmail.com>
2019-10-11 09:28:58 +0100

    refactored to remove check for empty list

--- challenge14/src/test/kotlin/MyFold.kt
+++ challenge14/src/test/kotlin/MyFold.kt
@@ -1,10 +1,7 @@
 fun myFold(list: List<Int>, acc:String, myFunction:(String,Int)->String): String {
-    if (list.isEmpty()) return ""
-    else {
-        var output = acc
-        for (item in list) {
-            output += "$item"
-        }
-        return output
+    var output = acc
+    for (item in list) {
+        output += "$item"
     }
+    return output
 }
