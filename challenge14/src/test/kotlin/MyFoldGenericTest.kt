001cfbc2d8808c615408db54e4f1435e0c13d8e3
Mike Neilens <mike.neilens@gmail.com>
2019-10-11 10:10:26 +0100

    added more tests to the generic fold tests

--- challenge14/src/test/kotlin/MyFoldGenericTest.kt
+++ challenge14/src/test/kotlin/MyFoldGenericTest.kt
@@ -13,4 +13,16 @@ class MyFoldGenericTest {
     fun `Empty list of String returns 0`() {
         Assertions.assertEquals(0, myFold(listOf<String>(), 0, convertListOfStringToInt))
     }
+    @Test
+    fun `List of one String containing "1" returns 1`() {
+        Assertions.assertEquals(1, myFold(listOf<String>("1"), 0, convertListOfStringToInt))
+    }
+    @Test
+    fun `List of one String containing ("1","2") returns 12`() {
+        Assertions.assertEquals(12, myFold(listOf<String>("1","2"), 0, convertListOfStringToInt))
+    }
+    @Test
+    fun `List of one String containing ("1","2") and an initial value of 3  returns 312`() {
+        Assertions.assertEquals(312, myFold(listOf<String>("1","2"), 3, convertListOfStringToInt))
+    }
 }
