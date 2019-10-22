6c5c6bc01403ba9e98413d35f8cbb71956f2302c
Mike Neilens <mike.neilens@gmail.com>
2019-10-22 19:53:07 +0100

    added tests and code to parse deliveriesToShops

--- challenge15/src/main/kotlin/Main.kt
+++ challenge15/src/main/kotlin/Main.kt
@@ -15,3 +15,26 @@ fun createListOfDiscountsForAnEAN(csvOfDiscountsForEachEAN: String): List<Discou
      csvOfDiscountsForEachEAN.split(",")
         .windowed(3,3)
         .mapNotNull{createDiscountForEANorNull(it)}
+
+data class DeliveryToAShop(val product: String, val EAN: String, val item: String, val caseSize: Int, val depot: String, val qtyDelivered:Int)
+
+fun createDeliveryToAShopOrNull(list: List<String>): DeliveryToAShop? {
+    if (list.size != 6)  return null
+
+    val caseSize = list[3].toIntOrNull()
+    val qtyDelivered = list[5].toIntOrNull()
+    return if ((caseSize == null) || (qtyDelivered == null))
+        null
+    else
+        DeliveryToAShop(list[0],list[1],list[2], caseSize, list[4], qtyDelivered)
+}
+
+fun createListOfDeliveryToShop(csvOfDeliveryToShop: String): List<DeliveryToAShop> =
+    csvOfDeliveryToShop.split(",")
+        .windowed(6,6)
+        .mapNotNull{createDeliveryToAShopOrNull(it)}
+
+fun <ObjectType:Any>createListOfObject(csvData: String, parameters:Int, objectCreator:(List<String>)->ObjectType?): List<ObjectType> =
+    csvData.split(",")
+        .windowed(parameters,parameters)
+        .mapNotNull{objectCreator(it)}
--- challenge15/src/test/kotlin/MainTest.kt
+++ challenge15/src/test/kotlin/MainTest.kt
@@ -49,4 +49,59 @@ class MainTest {
             DiscountForAnEAN("Chicken Soup","E10004",127.70))
         assertEquals(expectedResult, listOfDiscountForAnEAN )
     }
+    
+    @Test
+    fun `createDeliveryToAShop creates the object correctly when given a valid array of strings`() {
+        val dataForOneEAN = listOf("Carrot Soup","E110011","I118","72","Depot-A","7")
+        
+        assertEquals(DeliveryToAShop("Carrot Soup","E110011","I118",72,"Depot-A",7), createDeliveryToAShopOrNull(dataForOneEAN))
+    }
+
+    @Test
+    fun `createDeliveryToAShop creates a null if the data contains wrong number of elements`() {
+        val dataForOneEAN = listOf("Tomato Soup","E10001")
+        assertNull( createDeliveryToAShopOrNull(dataForOneEAN))
+    }
+    @Test
+    fun `createDeliveryToAShop creates a null if either qty is invalid`() {
+        val dataWithInvalidCaseSize = listOf("Carrot Soup","E110011","I118","x72","Depot-A","7")
+        assertNull( createDiscountForEANorNull(dataWithInvalidCaseSize))
+
+        val dataWithInvalidQtyDelivered = listOf("Carrot Soup","E110011","I118","72","Depot-A","xx7")
+        assertNull( createDiscountForEANorNull(dataWithInvalidQtyDelivered))
+    }
+
+    @Test
+    fun `an empty string creates an empty ListOfDeliveryToAShop`() {
+        val listOfDeliveryToAShop = createListOfDeliveryToShop("")
+        assertTrue(listOfDeliveryToAShop.isEmpty())
+    }
+
+    @Test
+    fun `a string containing CSV data for one DeliveryToAShop creates a ListOfDeliveryToAShop containing one element`() {
+        val listOfDeliveryToAShop = createListOfDeliveryToShop("Carrot Soup,E110011,I118,72,Depot-A,7")
+        assertTrue(listOfDeliveryToAShop.size == 1)
+        assertEquals(listOf(DeliveryToAShop("Carrot Soup","E110011","I118",72,"Depot-A",7)), listOfDeliveryToAShop )
+    }
+    @Test
+    fun `a string containing CSV data for several DeliveryToAShop creates a ListOfDeliveryToAShop containing several elements`() {
+        val listOfDeliveryToAShop = createListOfDeliveryToShop("Carrot Soup,E110013,I122,96,Depot-B,11,Carrot Soup,E110013,I122,96,Depot-C,13,Chicken Soup,E110004,I105,96,Depot-A,10")
+        val expectedResult = listOf (
+            DeliveryToAShop("Carrot Soup","E110013","I122",96,"Depot-B",11),
+            DeliveryToAShop("Carrot Soup","E110013","I122",96,"Depot-C",13),
+            DeliveryToAShop("Chicken Soup","E110004","I105",96,"Depot-A",10)
+        )
+        assertEquals(expectedResult, listOfDeliveryToAShop )
+    }
+    @Test
+    fun `a string containing CSV data for several DeliveryToAShop including an invalid deliveryQty creates a ListOfDeliveryToAShop containing several elements`() {
+        val listOfDeliveryToAShop = createListOfDeliveryToShop("Carrot Soup,E110013,I122,96,Depot-B,11,Carrot Soup,E110013,I122,x96,Depot-C,13,Chicken Soup,E110004,I105,96,Depot-A,10")
+        val expectedResult = listOf (
+            DeliveryToAShop("Carrot Soup","E110013","I122",96,"Depot-B",11),
+            DeliveryToAShop("Chicken Soup","E110004","I105",96,"Depot-A",10)
+        )
+        assertEquals(expectedResult, listOfDeliveryToAShop )
+    }
+
+
 }
