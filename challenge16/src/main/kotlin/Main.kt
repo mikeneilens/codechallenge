6edf9e32f71854bce829bd5b1431ac9f9024a8c7
Mike Neilens <mike.neilens@gmail.com>
2019-11-08 18:42:47 +0000

    refactored to use negativeCheck from the Roman class to determine any negatives

--- challenge16/src/main/kotlin/Main.kt
+++ challenge16/src/main/kotlin/Main.kt
@@ -1,18 +1,19 @@
 
 class Roman(val symbol:String, val value:Int, val negativeCheck:Regex )
 val romanOne = Roman( "I",1,"IV|IX".toRegex())
-val romanFive = Roman( "V",5,"".toRegex())
+val romanFive = Roman( "V",5,"Z".toRegex())
 val romanTen = Roman( "X",10,"XL|XC".toRegex())
-val romanFifty = Roman( "L",50,"".toRegex())
+val romanFifty = Roman( "L",50,"Z".toRegex())
 val romanHundred = Roman( "C",100,"CD|CM".toRegex())
-val romanFiveHundred = Roman( "D",500,"".toRegex())
-val romanThousand = Roman( "M",1000,"".toRegex())
+val romanFiveHundred = Roman( "D",500,"Z".toRegex())
+val romanThousand = Roman( "M",1000,"Z".toRegex())
 
 fun String.fromRomanToInt(): Int {
     if (this.isEmpty()) return 0
-    val minusOnes = if (this.contains("IV|IX".toRegex() )) -1 else 0
-    val minusTens = if (this.contains("XL|XC".toRegex() )) -10 else 0
-    val minusHundreds = if (this.contains("CD|CM".toRegex() )) -100 else 0
+    val minusOnes = if (this.contains(romanOne.negativeCheck )) -2 * romanOne.value else 0
+    val minusTens = if (this.contains(romanTen.negativeCheck )) -2 * romanTen.value else 0
+    val minusHundreds = if (this.contains(romanHundred.negativeCheck )) -2 * romanHundred.value else 0
+    val minusFives = if (this.contains(romanFive.negativeCheck )) -2 * romanFive.value else 0
     val ones = this.filter { it.toString() == romanOne.symbol }.count()
     val fives = this.filter { it.toString() == romanFive.symbol }.count() * 5
     val tens = this.filter { it.toString() == romanTen.symbol }.count() * 10
@@ -20,5 +21,5 @@ fun String.fromRomanToInt(): Int {
     val hundreds = this.filter { it.toString() == romanHundred.symbol }.count() * 100
     val fiveHundreds = this.filter { it.toString() == romanFiveHundred.symbol }.count() * 500
     val thousands = this.filter { it.toString() == romanThousand.symbol }.count() * 1000
-    return ones + minusOnes * 2 + fives + tens + minusTens * 2 + fifties + hundreds + minusHundreds * 2 + fiveHundreds + thousands
+    return ones + minusOnes + fives + tens + minusFives +  minusTens + fifties + hundreds + minusHundreds + fiveHundreds + thousands
 }
