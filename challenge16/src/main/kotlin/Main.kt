d61515e79d0c93fd5656262b6a399083538ce3c9
Mike Neilens <mike.neilens@gmail.com>
2019-11-08 19:02:24 +0000

    refactored to create a list of roman symbols and calculate value using a fold

--- challenge16/src/main/kotlin/Main.kt
+++ challenge16/src/main/kotlin/Main.kt
@@ -7,19 +7,11 @@ val romanFifty = Roman( "L",50,"Z".toRegex())
 val romanHundred = Roman( "C",100,"CD|CM".toRegex())
 val romanFiveHundred = Roman( "D",500,"Z".toRegex())
 val romanThousand = Roman( "M",1000,"Z".toRegex())
+val romanValues = listOf(romanOne,romanFive,romanTen,romanFifty, romanHundred,romanFiveHundred,romanThousand)
+
 
 fun String.fromRomanToInt(): Int {
-    if (this.isEmpty()) return 0
-    val minusOnes = if (this.contains(romanOne.negativeCheck )) -2 * romanOne.value else 0
-    val minusTens = if (this.contains(romanTen.negativeCheck )) -2 * romanTen.value else 0
-    val minusHundreds = if (this.contains(romanHundred.negativeCheck )) -2 * romanHundred.value else 0
-    val minusFives = if (this.contains(romanFive.negativeCheck )) -2 * romanFive.value else 0
-    val ones = this.filter { it.toString() == romanOne.symbol }.count()
-    val fives = this.filter { it.toString() == romanFive.symbol }.count() * 5
-    val tens = this.filter { it.toString() == romanTen.symbol }.count() * 10
-    val fifties = this.filter { it.toString() == romanFifty.symbol }.count() * 50
-    val hundreds = this.filter { it.toString() == romanHundred.symbol }.count() * 100
-    val fiveHundreds = this.filter { it.toString() == romanFiveHundred.symbol }.count() * 500
-    val thousands = this.filter { it.toString() == romanThousand.symbol }.count() * 1000
-    return ones + minusOnes + fives + tens + minusFives +  minusTens + fifties + hundreds + minusHundreds + fiveHundreds + thousands
+    return romanValues.fold(0){acc, element -> acc
+        acc + this.filter { it.toString() == element.symbol }.count() * element.value  + (if (this.contains(element.negativeCheck )) -2 * element.value else 0)
+    }
 }
