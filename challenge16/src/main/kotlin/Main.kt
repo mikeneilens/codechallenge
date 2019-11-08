4e75b026c3f05701049744153945d23c6f053873
Mike Neilens <mike.neilens@gmail.com>
2019-11-08 21:42:05 +0000

    refactored to use one list of roman symbols for translating numbers/symbols both ways

--- challenge16/src/main/kotlin/Main.kt
+++ challenge16/src/main/kotlin/Main.kt
@@ -1,27 +1,25 @@
 
-class Roman(val symbol:String, val value:Int, val negativeCheck:Regex = "Z".toRegex() )
-val romanOne = Roman( "I",1,"IV|IX".toRegex())
-val romanFour = Roman( "IV",4)
-val romanFive = Roman( "V",5)
-val romanNine = Roman( "IX",9)
-val romanTen = Roman( "X",10,"XL|XC".toRegex())
-val romanForty = Roman( "XL",40)
-val romanFifty = Roman( "L",50)
-val romanNinety = Roman( "XC",90)
-val romanHundred = Roman( "C",100,"CD|CM".toRegex())
-val romanFourHundred = Roman( "CD",400)
-val romanFiveHundred = Roman( "D",500)
-val romanNineHundred = Roman( "CM",900)
-val romanThousand = Roman( "M",1000)
+class Roman(val symbol: String, val value: Int, val negativeCheck: Regex = "Z".toRegex())
+val romanOne = Roman("I", 1, "IV|IX".toRegex())
+val romanFour = Roman("IV", 4)
+val romanFive = Roman("V", 5)
+val romanNine = Roman("IX", 9)
+val romanTen = Roman("X", 10, "XL|XC".toRegex())
+val romanForty = Roman("XL", 40)
+val romanFifty = Roman("L", 50)
+val romanNinety = Roman("XC", 90)
+val romanHundred = Roman("C", 100, "CD|CM".toRegex())
+val romanFourHundred = Roman("CD", 400)
+val romanFiveHundred = Roman("D", 500)
+val romanNineHundred = Roman("CM", 900)
+val romanThousand = Roman("M", 1000)
 
-val romanValues = listOf(romanOne,romanFive,romanTen,romanFifty, romanHundred,romanFiveHundred,romanThousand)
 val romanSymbols = listOf(romanThousand,romanNineHundred,romanFiveHundred,romanFourHundred,romanHundred, romanNinety, romanFifty,romanForty,romanTen,romanNine,romanFive,romanFour,romanOne)
 
-fun String.fromRomanToInt(): Int =
-     romanValues.fold(0){acc, element -> acc
+fun String.fromRomanSymbolToInt(): Int =
+     romanSymbols.fold(0){acc, element -> acc
         acc + this.filter { it.toString() == element.symbol }.count() * element.value  + (if (this.contains(element.negativeCheck )) -2 * element.value else 0)
      }
-
 
 fun Int.fromIntToRomanSymbol(): String =
     romanSymbols.fold(Pair("",this)){ (result,remainder),roman ->
--- challenge16/src/test/kotlin/MainTest.kt
+++ challenge16/src/test/kotlin/MainTest.kt
@@ -4,95 +4,95 @@ import org.junit.jupiter.api.Test
 class MainTest {
     @Test
     fun `string containing nothing is converted to 0`() {
-        assertEquals(0, "".fromRomanToInt())
+        assertEquals(0, "".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing I is converted to 1`() {
-        assertEquals(1, "I".fromRomanToInt())
+        assertEquals(1, "I".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing II is converted to 2`() {
-        assertEquals(2, "II".fromRomanToInt())
+        assertEquals(2, "II".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing III is converted to 3`() {
-        assertEquals(3, "III".fromRomanToInt())
+        assertEquals(3, "III".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing IV is converted to 4`() {
-        assertEquals(4, "IV".fromRomanToInt())
+        assertEquals(4, "IV".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing V is converted to 5`() {
-        assertEquals(5, "V".fromRomanToInt())
+        assertEquals(5, "V".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing VI is converted to 6`() {
-        assertEquals(6, "VI".fromRomanToInt())
+        assertEquals(6, "VI".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing VII is converted to 7`() {
-        assertEquals(7, "VII".fromRomanToInt())
+        assertEquals(7, "VII".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing X is converted to 10`() {
-        assertEquals(10, "X".fromRomanToInt())
+        assertEquals(10, "X".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing XIII is converted to 13`() {
-        assertEquals(13, "XIII".fromRomanToInt())
+        assertEquals(13, "XIII".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing XIV is converted to 14`() {
-        assertEquals(14, "XIV".fromRomanToInt())
+        assertEquals(14, "XIV".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing XVIII is converted to 18`() {
-        assertEquals(18, "XVIII".fromRomanToInt())
+        assertEquals(18, "XVIII".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing IX is converted to 9`() {
-        assertEquals(9, "IX".fromRomanToInt())
+        assertEquals(9, "IX".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing L is converted to 50`() {
-        assertEquals(50, "L".fromRomanToInt())
+        assertEquals(50, "L".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing XL is converted to 40`() {
-        assertEquals(40, "XL".fromRomanToInt())
+        assertEquals(40, "XL".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing C is converted to 100`() {
-        assertEquals(100, "C".fromRomanToInt())
+        assertEquals(100, "C".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing XC is converted to 90`() {
-        assertEquals(90, "XC".fromRomanToInt())
+        assertEquals(90, "XC".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing CCLXXVII is converted to 277`() {
-        assertEquals(277, "CCLXXVII".fromRomanToInt())
+        assertEquals(277, "CCLXXVII".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing D is converted to 500`() {
-        assertEquals(500, "D".fromRomanToInt())
+        assertEquals(500, "D".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing CD is converted to 400`() {
-        assertEquals(400, "CD".fromRomanToInt())
+        assertEquals(400, "CD".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing M is converted to 1000`() {
-        assertEquals(1000, "M".fromRomanToInt())
+        assertEquals(1000, "M".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing MM is converted to 2000`() {
-        assertEquals(2000, "MM".fromRomanToInt())
+        assertEquals(2000, "MM".fromRomanSymbolToInt())
     }
     @Test
     fun `string containing MCM is converted to 1900`() {
-        assertEquals(1900, "MCM".fromRomanToInt())
+        assertEquals(1900, "MCM".fromRomanSymbolToInt())
     }
 
     @Test
