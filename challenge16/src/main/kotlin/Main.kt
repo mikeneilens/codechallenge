f1c6742332b4da14562de9e88bbffc7e0943f527
Mike Neilens <mike.neilens@gmail.com>
2019-11-18 10:59:51 +0000

    minor refactor

--- challenge16/src/main/kotlin/Main.kt
+++ challenge16/src/main/kotlin/Main.kt
@@ -5,8 +5,7 @@ fun addRomanNumbers(first:String, second:String):String = (first.fromRomanSymbol
 val romanKeyValues = mapOf("M" to 1000, "CM" to 900, "D" to 500,"CD" to 400, "C" to 100, "XC" to 90, "L" to 50, "XL" to 40, "X" to 10, "IX" to 9, "V" to 5, "IV" to 4, "I" to 1 )
 
 val String.fromRomanSymbolToInt:Int get() {
-    val result:Int =  this.toList().map{it.toString()}.foldRight(Pair(0,0),{ romanDigit, acc ->
-        val (total, valueOfLastRomanDigit) = acc
+    val result:Int =  this.toList().map{it.toString()}.foldRight(Pair(0,0),{ romanDigit, (total, valueOfLastRomanDigit) ->
         val valueOfRomanDigit = romanKeyValues[romanDigit] ?: 0
         if (valueOfRomanDigit >= valueOfLastRomanDigit) Pair(total + valueOfRomanDigit, valueOfRomanDigit) else Pair(total - valueOfRomanDigit, valueOfRomanDigit)
     }).first
@@ -14,8 +13,7 @@ val String.fromRomanSymbolToInt:Int get() {
 }
 
 val Int.fromIntToRomanSymbol: String get() =
-    romanKeyValues.toList().fold(Pair("",this)){ (result,remainder),romanKeyValue ->
-        val (symbol, value) = romanKeyValue
+    romanKeyValues.toList().fold(Pair("",this)){ (result,remainder),(symbol, value) ->
         val multiplesOfTheValue = remainder / value
         Pair(result + symbol.repeat(multiplesOfTheValue), remainder - multiplesOfTheValue * value)
     }.first
