0f1648295c83093cc729e4f0241629ccf13bfa37
Mike Neilens <mike.neilens@gmail.com>
2020-01-08 19:22:55 +0000

    challenge 19

--- challenge19/src/main/kotlin/Main.kt
+++ challenge19/src/main/kotlin/Main.kt
@@ -4,16 +4,14 @@ data class Position(val x:Int, val y:Int) {
     operator fun plus(other:Position) = Position(x + other.x, y + other.y)
 }
 
+enum class Command { M,L,R }
+
+enum class Output(val symbol:String) { Left("L"),Right("R"),Open("O") }
+
 val moveNorth = Position(0,-1)
 val moveSouth = Position(0,1)
 val moveWest = Position(-1,0)
 val moveEast = Position(1,0)
-
-enum class Command { M,L,R }
-
-enum class Output(val symbol:String) {
-    Left("L"),Right("R"),Open("O")
-}
 
 enum class Orientation (val value:Int, val ahead:Position, val left:Position, val right:Position) {
     East(1, moveEast, moveNorth, moveSouth),
@@ -43,6 +41,46 @@ enum class Orientation (val value:Int, val ahead:Position, val left:Position, va
     }
  }
 
+data class Trolley(val position:Position, val orientation: Orientation, val trolleyId:Int) {
+    val positionAhead = position + orientation.ahead
+    val positionToLeft = position + orientation.left
+    val positionToRight = position + orientation.right
+
+    fun move() = Trolley (positionAhead, orientation, trolleyId)
+    fun rotateLeft() = Trolley(position, orientation.turnLeft(), trolleyId)
+    fun rotateRight() = Trolley(position, orientation.turnRight(), trolleyId)
+
+    fun toReferenceId(): String =   hashids.encode(position.x.toLong() + 1, position.y.toLong() + 1, orientation.value.toLong(), trolleyId.toLong())
+
+    fun viewAhead(mapData:String):List<String>{
+        var movedTrolley = this
+        var result = mutableListOf<String>()
+        while ( mapData.contentAt( movedTrolley.positionAhead) != Content.Fixture ) {
+            movedTrolley  = movedTrolley.move()
+            var stringForPosition = Output.Open.symbol
+            if (mapData.contentAt( movedTrolley.positionToLeft) == Content.Open) stringForPosition += Output.Left.symbol
+            if (mapData.contentAt( movedTrolley.positionToRight) == Content.Open) stringForPosition += Output.Right.symbol
+            result.add(stringForPosition)
+        }
+        return result
+    }
+
+    fun moveOrRotate(command:Command, mapData:String) =
+        when (command) {
+            Command.M ->if (mapData.contentAt(positionAhead) != Content.Fixture) move() else this
+            Command.L -> rotateLeft()
+            Command.R -> rotateRight()
+        }
+
+    companion object {
+        private val hashids = Hashids(SALT_FOR_HASH)
+        fun from(hash:String):Trolley {
+            val decoded = hashids.decode(hash)
+            return Trolley(Position(decoded[0].toInt() - 1,decoded[1].toInt() - 1),Orientation.from(decoded[2].toInt()),decoded[3].toInt())
+        }
+    }
+}
+
 enum class Content (val symbol:Char) {
     Open(' '),
     Fixture('*');
@@ -61,55 +99,44 @@ fun String.contentAt(position:Position):Content {
     return Content.from(rowChars[position.x])
 }
 
-fun viewAhead(position:Position, orientation:Orientation, mapData:String):List<String>{
-    var newPosition = position
-    var result = mutableListOf<String>()
-    while ( mapData.contentAt( newPosition + orientation.ahead) != Content.Fixture ) {
-        newPosition += orientation.ahead
-        var stringForPosition = Output.Open.symbol
-        if (mapData.contentAt( newPosition + orientation.left) == Content.Open) stringForPosition += Output.Left.symbol
-        if (mapData.contentAt( newPosition + orientation.right) == Content.Open) stringForPosition += Output.Right.symbol
-        result.add(stringForPosition)
-    }
-    return result
-}
+fun moveTrolley(command:Command, referenceId:String, _mapData:String): Pair<List<String>, String>{
+    val mapData = if (_mapData.isEmpty()) defaultMapData else _mapData
 
-fun moveTrolley(command:Command, referenceId:String, mapData:String): Pair<List<String>, String>{
-    val (position, orientation, trollyId) = UserDetails.from(referenceId)
-
-    val (newPosition:Position, newOrientation) = moveOrRotate(command,position,orientation,mapData)
-
-    val viewAhead = viewAhead(newPosition, newOrientation, mapData)
-
-    val newReferenceId = UserDetails(newPosition, newOrientation, trollyId).toReferenceId()
+    val trolley = Trolley.from(referenceId)
+    val newTrolley = trolley.moveOrRotate(command,mapData)
+    val viewAhead = newTrolley.viewAhead(mapData)
+    val newReferenceId = newTrolley.toReferenceId()
     return Pair(viewAhead, newReferenceId )
 }
 
-fun moveOrRotate(command:Command, position:Position, orientation:Orientation, mapData:String) =
-    when (command) {
-        Command.M ->if (mapData.contentAt(position + orientation.ahead) != Content.Fixture)
-                         Pair (position + orientation.ahead, orientation)
-                    else Pair(position, orientation)
-        Command.L -> Pair(position, orientation.turnLeft())
-        Command.R -> Pair(position, orientation.turnRight())
-    }
 
-fun moveTrolley(mapData:String):Pair<List<String>,String> {
-    val x = 1
-    val y = 1
+fun moveTrolley(_mapData:String):Pair<List<String>,String> {
+    val mapData = if (_mapData.isEmpty()) defaultMapData else _mapData
+    val startingPosition = mapData.startingPosition()
     val orientation = Orientation.East
-    val viewAhead = viewAhead(Position(x,y), orientation, mapData)
-    val referenceId = UserDetails(Position(x,y),orientation,1234).toReferenceId()
+    val trolleyId = 1234
+    val trolley = Trolley(startingPosition,orientation, trolleyId)
+    val viewAhead = trolley.viewAhead(mapData)
+    val referenceId = trolley.toReferenceId()
     return Pair(viewAhead, referenceId)
 }
 
-data class UserDetails(val position:Position, val orentiation:Orientation, val trollyId:Int) {
-    fun toReferenceId(): String =   hashids.encode(position.x.toLong(), position.y.toLong(), orentiation.value.toLong(), trollyId.toLong())
-    companion object {
-        private val hashids = Hashids("a random string")
-        fun from(hash:String):UserDetails {
-            val decoded = hashids.decode(hash)
-            return UserDetails( Position(decoded[0].toInt(),decoded[1].toInt()),Orientation.from(decoded[2].toInt()),decoded[3].toInt())
-        }
-    }
-}
+fun String.startingPosition() = split("\n")
+                                        .mapIndexed{y, row -> row.toList().mapIndexed{x, char -> Pair(Position(x,y),char) } }
+                                        .flatMap{it}
+                                        .first{it.second == Content.Open.symbol}.first
+
+
+val SALT_FOR_HASH  = "a random string"
+
+val defaultMapData =
+        "\"**************************\\n\" +\n" +
+        "\"*                        *\\n\" +\n" +
+        "\"* ********** *********** *\\n\" +\n" +
+        "\"* ********** *********** *\\n\" +\n" +
+        "\"*                        *\\n\" +\n" +
+        "\"* ********** *********** *\\n\" +\n" +
+        "\"* ********** *********** *\\n\" +\n" +
+        "\"* ********** *********** *\\n\" +\n" +
+        "\"*                        *\\n\" +\n" +
+        "\"**************************\\n\" +\n"
--- challenge19/src/test/kotlin/MainTest.kt
+++ challenge19/src/test/kotlin/MainTest.kt
@@ -31,78 +31,77 @@ class MainTest {
 
     @Test
     fun `view ahead is empty if map is one character`() {
-        val position = Position(0,0)
-        val orientation = Orientation.East
+        val trolley = Trolley(Position(0,0),Orientation.East,5312)
         val mapData = " "
-        assertEquals(listOf<String>(), viewAhead(position,orientation,mapData))
+        assertEquals(listOf<String>(), trolley.viewAhead(mapData))
     }
 
     @Test
     fun `view ahead is one O if map is two characters and trolley is facing towards a space from any direction`() {
-        val positionFacingEast = Position(0,0)
+        val trolleyEast = Trolley(Position(0,0),Orientation.East,5312)
         val mapDataForEast = "  "
-        assertEquals(listOf<String>("O"), viewAhead(positionFacingEast,Orientation.East,mapDataForEast))
+        assertEquals(listOf<String>("O"), trolleyEast.viewAhead(mapDataForEast))
 
-        val positionFacingWest = Position(1,0)
+        val trolleyWest= Trolley(Position(1,0),Orientation.West,5312)
         val mapDataForWest = "  "
-        assertEquals(listOf<String>("O"), viewAhead(positionFacingWest,Orientation.West,mapDataForWest))
+        assertEquals(listOf<String>("O"), trolleyWest.viewAhead(mapDataForWest))
 
-        val positionFacingSouth = Position(0,0)
+        val trolleySouth = Trolley(Position(0,0),Orientation.South,5312)
         val mapDataForSouth = "  \n" + " *"
-        assertEquals(listOf<String>("O"), viewAhead(positionFacingSouth,Orientation.South,mapDataForSouth))
+        assertEquals(listOf<String>("O"), trolleySouth.viewAhead(mapDataForSouth))
 
-        val positionFacingNorth = Position(0,1)
+        val trolleyNorth = Trolley(Position(0,1),Orientation.North,5312)
         val mapDataForNorth = " *\n" + "  "
-        assertEquals(listOf<String>("O"), viewAhead(positionFacingNorth,Orientation.North,mapDataForNorth))
+        assertEquals(listOf<String>("O"), trolleyNorth.viewAhead(mapDataForNorth))
     }
 
     @Test
     fun `view ahead is more than one O if trolley is facing into a dead end corridor of more than one space`() {
-        val position = Position(1,0)
+        val trolley = Trolley(Position(1,0),Orientation.South,5312)
         val mapData = "* *\n" + "* *\n" + "* *\n" + "* *\n"
-        assertEquals(listOf("O","O","O"), viewAhead(position,Orientation.South,mapData))
+        assertEquals(listOf("O","O","O"), trolley.viewAhead(mapData))
     }
 
     @Test
     fun `view ahead contains a left turn if there is a left turn`() {
-        val position = Position(1,0)
+        val trolley = Trolley(Position(1,0),Orientation.South,5312)
         val mapData = "* *\n" + "* *\n" + "*  \n" + "* *\n"
-        assertEquals(listOf("O","OL","O"), viewAhead(position,Orientation.South,mapData))
+        assertEquals(listOf("O","OL","O"), trolley.viewAhead(mapData))
     }
 
     @Test
     fun `view ahead contains a right turn if there is a right turn`() {
-        val position = Position(1,0)
+        val trolley = Trolley(Position(1,0),Orientation.South,5312)
         val mapData = "* *\n" + "* *\n" + "  *\n" + "* *\n"
-        assertEquals(listOf("O","OR","O"), viewAhead(position,Orientation.South,mapData))
+        assertEquals(listOf("O","OR","O"), trolley.viewAhead(mapData))
     }
 
     @Test
     fun `view ahead contains a right and left turn if there is both`() {
-        val position = Position(1,0)
+        val trolley = Trolley(Position(1,0),Orientation.South,5312)
         val mapData = "* *\n" + "*  \n" + "  *\n" + "* *\n"
-        assertEquals(listOf("OL","OR","O"), viewAhead(position,Orientation.South,mapData))
+        assertEquals(listOf("OL","OR","O"), trolley.viewAhead(mapData))
     }
 
     @Test
     fun `view ahead contains a right and left turn and a cross roads if there is all three`() {
-        val position = Position(1,0)
+        val trolley = Trolley(Position(1,0),Orientation.South,5312)
         val mapData = "* *\n" + "*  \n" + "  *\n" + "   \n" + "* *\n"
-        assertEquals(listOf("OL","OR","OLR","O"), viewAhead(position,Orientation.South,mapData))
+        assertEquals(listOf("OL","OR","OLR","O"), trolley.viewAhead(mapData))
     }
 
     @Test
     fun `attempting to move a trolley into a fixture leaves the trolley in the same position`() {
-        val position = Position(0,0)
+        val trolley = Trolley(Position(0,0),Orientation.East,5312)
         val mapData = " *"
-        val (newPosition, newOrientation) = moveOrRotate(Command.M,position, Orientation.East, mapData)
-        assertEquals(position, newPosition)
+        val newTrolley = trolley.moveOrRotate(Command.M,mapData)
+        assertEquals(trolley.position, newTrolley.position)
     }
     @Test
     fun `rotating the trolley facing East Left leaves it facing North` () {
-        val position = Position(0,0)
+        val trolley = Trolley(Position(0,0),Orientation.East,5312)
         val mapData = " *"
-        val (newPosition, newOrientation) = moveOrRotate(Command.L,position, Orientation.East, mapData)
+        val (newPosition, newOrientation) = trolley.moveOrRotate(Command.L,mapData)
         assertEquals(Orientation.North, newOrientation)
     }
 
@@ -110,7 +109,7 @@ class MainTest {
     fun `calling function to move trolley with no initial referenceId returns a view from an initial position of 1,1`() {
         val mapData =
                             "************\n" +
-                            "           *\n" +
+                            "*          *\n" +
                             "************\n"
         val (viewAhead, referenceId ) = moveTrolley(mapData)
         assertEquals(listOf("O", "O", "O", "O", "O", "O", "O", "O", "O"),viewAhead)
@@ -124,8 +123,8 @@ class MainTest {
         val (viewAhead, referenceId ) = moveTrolley(mapData)
         val (nextViewAhead, nextReferenceId) = moveTrolley(Command.M, referenceId, mapData)
         assertEquals(viewAhead.size - 1, nextViewAhead.size)
-        assertEquals(listOf("O", "O", "O", "O", "O", "O", "O", "O", "O"),viewAhead)
-        assertEquals(listOf("O", "O", "O", "O", "O", "O", "O", "O"),nextViewAhead)
+        assertEquals(listOf("O", "O", "O", "O", "O", "O", "O", "O", "O", "O"),viewAhead)
+        assertEquals(listOf("O", "O", "O", "O", "O", "O", "O", "O", "O"),nextViewAhead)
     }
 
 }
