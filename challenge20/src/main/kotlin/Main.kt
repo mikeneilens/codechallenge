b98b3e3027d52de2006245f82e0ab1506637c2e7
Mike Neilens <mike.neilens@gmail.com>
2020-01-24 12:57:14 +0000

    updated with basic functions

--- challenge20/src/main/kotlin/Main.kt
+++ challenge20/src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 import java.net.URL
 
-fun httpRequest(url:String):String
-    = try {
+typealias Httprequest = (String) -> String
+
+fun httpRequest(url:String):String = try {
     URL(url)
         .openStream()
         .bufferedReader()
@@ -9,3 +10,16 @@ fun httpRequest(url:String):String
     } catch (e:Error) {
         ""
     }
+
+data class MapResponse (val referenceId:String, val points:List<String>)
+
+fun parseResponse(text:String): MapResponse {
+    val splitData = text.split(",")
+    return MapResponse(splitData.first(), splitData.drop(1))
+}
+
+enum class Command{ M,L,R }
+
+fun decideAction(mapResponse: MapResponse):Command {
+    return Command.M
+}
