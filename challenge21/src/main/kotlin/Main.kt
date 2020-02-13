d55f33843836509061002366a0128b1f97f5e8d6
Mike Neilens <mike.neilens@gmail.com>
2020-02-13 21:31:41 +0000

    updated to solve harer sudokus

--- challenge21/src/main/kotlin/Main.kt
+++ challenge21/src/main/kotlin/Main.kt
@@ -22,6 +22,9 @@ fun SudokuGrid.potentialNumbers(index:Int):Set<Int> {
     return (1..9).mapNotNull { if (numbersAlreadyUsed.contains(it)) null else it }.toSet()
 }
 
+fun SudokuGrid.indexesWithMoreThanOneAnswer() = mapIndexedNotNull()
+        {index, value -> if (value == 0) Pair(index, potentialNumbers(index)) else null }
+
 fun SudokuGrid.numberToReplaceZero(index:Int):Int {
     val potentialNumbers = potentialNumbers(index)
     return if (potentialNumbers.size == 1) potentialNumbers.first() else 0
@@ -33,4 +36,41 @@ fun SudokuGrid.completeSoduku():SudokuGrid {
     val newSudoku = replaceZeros()
     if (this == newSudoku) return newSudoku
     return newSudoku.completeSoduku()
+}
+
+fun SudokuGrid.completeAllSodoku(completed:List<SudokuGrid> = listOf()):List<SudokuGrid> {
+    val sudoku = completeSoduku()
+
+    if (!sudoku.contains(0))
+        return if (sudoku.isLegitimate()) completed + listOf(sudoku)
+        else completed
+
+    val indexesWithMoreThanOneAnswer = sudoku.indexesWithMoreThanOneAnswer()
+
+    if (indexesWithMoreThanOneAnswer.isEmpty()) return completed
+
+    val (indexToTry, solutionsToTry) = indexesWithMoreThanOneAnswer.sortedBy { it.second.size }.first()
+
+    return solutionsToTry.flatMap{solution ->
+        val newSudoku = sudoku.mapIndexed{index, value -> if (index == indexToTry) solution else value }
+        newSudoku.completeAllSodoku(completed)
+    }
+}
+
+fun SudokuGrid.print() {
+    println(this.chunked(9)[0])
+    println(this.chunked(9)[1])
+    println(this.chunked(9)[2])
+    println(this.chunked(9)[3])
+    println(this.chunked(9)[4])
+    println(this.chunked(9)[5])
+    println(this.chunked(9)[6])
+}
+
+fun SudokuGrid.isLegitimate():Boolean {
+    var legitimate = true
+    (0..8).forEach{ if  (numbersInRow(it).intersect( setOf(1,2,3,4,5,6,7,8,9)).size < 9 )  legitimate = false   }
+    (0..8).forEach{ if  (numbersInCol(it).intersect( setOf(1,2,3,4,5,6,7,8,9)).size < 9) legitimate = false   }
+    (0..8).forEach{ if  (numbersInRegion(it).intersect( setOf(1,2,3,4,5,6,7,8,9)).size < 9) legitimate = false   }
+    return legitimate
 }
--- challenge21/src/test/kotlin/MainTest.kt
+++ challenge21/src/test/kotlin/MainTest.kt
@@ -1,27 +1,28 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class MainTest {
-    val data = listOf(
+    val easyProblem = listOf(
         7,0,9,0,0,2,6,8,0,
         0,0,2,0,5,0,7,0,4,
         0,0,0,0,0,0,2,0,0,
         1,9,0,0,0,7,0,6,0,
         8,6,7,1,9,5,0,4,0,
-        5,0,4,0,0,0,0,9,9,
+        5,0,4,0,0,0,0,9,0,
         4,3,5,7,8,0,0,2,0,
         0,0,6,4,0,0,0,0,1,
         9,8,0,5,0,6,0,0,3)
 
     @Test
     fun `numberInRow should return the numbers in a row`() {
-        val result = data.numbersInRow(1)
+        val result = easyProblem.numbersInRow(1)
         val expectedResult = setOf(0,0,2,0,5,0,7,0,4)
         assertEquals(expectedResult,result)
     }
     @Test
     fun `numbersInCol should return the numbers in a column`() {
-        val result = data.numbersInCol(1)
+        val result = easyProblem.numbersInCol(1)
         val expectedResult = setOf(0,0,0,9,6,0,3,0,8)
         assertEquals(expectedResult,result)
     }
@@ -34,69 +35,136 @@ class MainTest {
     }
     @Test
     fun `numbersInRefion should return the numbers in the region`() {
-        val result0 = data.numbersInRegion(0)
+        val result0 = easyProblem.numbersInRegion(0)
         val expectedResult0 = setOf(7,0,9,0,0,2,0,0,0)
         assertEquals(expectedResult0,result0)
 
-        val result4 = data.numbersInRegion(4)
+        val result4 = easyProblem.numbersInRegion(4)
         val expectedResult4 = setOf(0,0,7,1,9,5,0,0,0)
         assertEquals(expectedResult4,result4)
 
-        val result8 = data.numbersInRegion(8)
+        val result8 = easyProblem.numbersInRegion(8)
         val expectedResult8 = setOf(0,2,0,0,0,1,0,0,3)
         assertEquals(expectedResult8,result8)
     }
     @Test
     fun `nmubersAlreadyUsed should be all of the numbers already in a column, row and region of an index `() {
-        val result = data.numbersAlreadyUsed(1)
+        val result = easyProblem.numbersAlreadyUsed(1)
         val expectedResult = setOf(0, 9, 6, 3, 8, 7, 2)
         assertEquals(expectedResult,result)
     }
     @Test
     fun `potentialNumbers should be any numbers between 1 and 9 not already used`() {
-        val result = data.potentialNumbers(1)
+        val result = easyProblem.potentialNumbers(1)
         val expectedResult = setOf(1,4,5)
         assertEquals(expectedResult,result)
     }
     @Test
     fun `number to replace zero is the only potential number or else its zero`() {
-        val zeroResult = data.numberToReplaceZero(1)
-        val nonZeroResult = data.numberToReplaceZero(42)
+        val zeroResult = easyProblem.numberToReplaceZero(1)
+        val nonZeroResult = easyProblem.numberToReplaceZero(42)
 
         assertEquals(0, zeroResult)
         assertEquals(3, nonZeroResult)
     }
     @Test
     fun `replacing zeros with numbers after one pass gives a correct sudoku`() {
-        val result = data.replaceZeros()
+        val result = easyProblem.replaceZeros()
         val expectedResult = listOf(
             7,0,9,3,0,2,6,8,5,
             0,1,2,0,5,0,7,0,4,
-            0,0,0,0,0,0,2,0,5,
+            0,0,0,0,0,0,2,0,0,
             1,9,3,0,0,7,0,6,0,
             8,6,7,1,9,5,3,4,2,
-            5,2,4,0,0,0,0,9,9,
-            4,3,5,7,8,0,9,2,6,
+            5,2,4,0,0,0,0,9,0,
+            4,3,5,7,8,0,9,2,0,
             2,0,6,4,0,0,0,0,1,
             9,8,1,5,0,6,4,7,3)
 
         assertEquals(expectedResult,result)
     }
+
     @Test
     fun `completeSudoku should complete it or return one with zeros for places where it is not possible to complete without exploring permutations`() {
-        val result = data.completeSoduku()
+        val result = easyProblem.completeSoduku()
         val expectedResult = listOf(
             7,4,9,3,1,2,6,8,5,
-            6,1,2,8,5,8,7,3,4,
-            3,4,8,6,7,9,2,1,5,
+            6,1,2,9,5,8,7,3,4,
+            3,5,8,6,7,4,2,1,9,
             1,9,3,2,4,7,5,6,8,
             8,6,7,1,9,5,3,4,2,
-            5,2,4,8,6,3,1,9,9,
+            5,2,4,8,6,3,1,9,7,
             4,3,5,7,8,1,9,2,6,
             2,7,6,4,3,9,8,5,1,
             9,8,1,5,2,6,4,7,3
         )
         assertEquals(expectedResult,result)
+    }
+    @Test
+    fun `completeAllSudoku should give an empty list if there are no solutions`() {
+        val unsolvable = listOf(
+            7,8,1,5,4,3,9,2,6,
+            0,0,6,1,7,9,5,0,0,
+            9,5,4,6,2,8,7,3,1,
+            6,9,5,8,3,7,2,1,4,
+            1,4,8,2,6,5,3,7,9,
+            3,2,7,9,1,4,8,0,0,
+            4,1,3,7,5,2,6,9,8,
+            0,0,2,0,0,0,4,0,0,
+            5,7,9,4,8,6,1,0,3
+        )
+        val result = unsolvable.completeAllSodoku()
+        val expectedResult = listOf<SudokuGrid>()
+        assertEquals(expectedResult,result)
+    }
+
+    @Test
+    fun `completeAllSudoku should solve a hard problem with one answer`() {
+        val hardProblem = listOf(
+            0,0,0,0,7,4,3,1,6,
+            0,0,0,6,0,3,8,4,0,
+            0,0,0,0,0,8,5,0,0,
+            7,2,5,8,0,0,0,3,4,
+            0,0,0,0,3,0,0,5,0,
+            0,0,0,0,0,2,7,9,8,
+            0,0,8,9,4,0,0,0,0,
+            0,4,0,0,8,5,9,0,0,
+            9,7,1,3,2,6,4,8,5
+        )
+        val result = hardProblem.completeAllSodoku()
+        val expectedResult = listOf(listOf(
+            5,8,9,2,7,4,3,1,6,
+            2,1,7,6,5,3,8,4,9,
+            4,6,3,1,9,8,5,2,7,
+            7,2,5,8,1,9,6,3,4,
+            8,9,6,4,3,7,1,5,2,
+            1,3,4,5,6,2,7,9,8,
+            6,5,8,9,4,1,2,7,3,
+            3,4,2,7,8,5,9,6,1,
+            9,7,1,3,2,6,4,8,5
+        ))
+        assertEquals(expectedResult,result)
+    }
+    @Test
+    fun `completeAllSudoku should solve a hard problem with more than one answer`() {
+        val multipleAnswerProblem = listOf(
+            0,8,0,0,0,9,7,4,3,
+            0,5,0,0,0,8,0,1,0,
+            0,1,0,0,0,0,0,0,0,
+            8,0,0,0,0,5,0,0,0,
+            0,0,0,8,0,4,0,0,0,
+            0,0,0,3,0,0,0,0,6,
+            0,0,0,0,0,0,0,7,0,
+            0,3,0,5,0,0,0,8,0,
+            9,7,2,4,0,0,0,5,0
+        )
+
+        val results = multipleAnswerProblem.completeAllSodoku()
+
+        results.forEachIndexed { index, result ->
+            assertTrue(result.isLegitimate())
+        }
+
     }
 }
 
