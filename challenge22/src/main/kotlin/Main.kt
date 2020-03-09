7bc1ab3793c12c1b1b26a9512cf19788aa26e343
Mike Neilens <mike.neilens@gmail.com>
2020-03-09 21:29:15 +0000

    added different upgrades and validation to prevent an upgrade being added twice

--- challenge22/src/main/kotlin/Main.kt
+++ challenge22/src/main/kotlin/Main.kt
@@ -1,28 +1,63 @@
 interface Laptop {
-    fun description():String
+    override fun toString():String
+    val price get():Int = 0
 }
 
-class BasicLaptop : Laptop {
-    override fun description() = "Basic Laptop"
+class BasicLaptop(override val price:Int) : Laptop {
+    override fun toString() = "Basic Laptop (£$price)"
 }
 
-abstract class LaptopDecorator(val decoratedLaptop: Laptop) : Laptop {
-    override fun description() = decoratedLaptop.description()
+abstract class LaptopDecorator  (val decoratedLaptop: Laptop,  val optionPrice:Int, val name:String, val description:String): Laptop {
+    override fun toString(): String  = "$decoratedLaptop + $name $description (£$optionPrice)"
+    override val price = decoratedLaptop.price + optionPrice
+
+    init {
+        if (upgradeAlreadyAdded(decoratedLaptop)) throw Exception("Trying to add ${this::class} to $decoratedLaptop.  The laptop already has a ${this::class}")
+    }
 }
 
-class ProcessUpgrade(decoratedLaptop: Laptop) : LaptopDecorator(decoratedLaptop) {
-    override fun description() = decoratedLaptop.description() + " + Better processor"
+fun LaptopDecorator.upgradeAlreadyAdded(laptopDecorator: Laptop):Boolean {
+    if (this::class == laptopDecorator::class) return true
+    return when (laptopDecorator) {
+        is LaptopDecorator -> upgradeAlreadyAdded(laptopDecorator.decoratedLaptop)
+        is Laptop -> false
+        else -> false
+    }
 }
 
-class MemoryUpgradeDecorator(decoratedLaptop: Laptop) : LaptopDecorator(decoratedLaptop) {
-    override fun description():String = decoratedLaptop.description() + " + Better memory"
+class ProcessorUpgrade(decoratedLaptop: Laptop, processorOption: ProcessorOption)
+    : LaptopDecorator(decoratedLaptop, processorOption.price, processorOption.description, "Processor")
+
+class MemoryUpgrade(decoratedLaptop: Laptop, memoryOption: MemoryOption )
+    : LaptopDecorator(decoratedLaptop, memoryOption.price, memoryOption.description, "Memory")
+
+class DriveUpgrade(decoratedLaptop: Laptop, driveOption: DriveOption )
+    : LaptopDecorator(decoratedLaptop, driveOption.price, driveOption.description, "SSD")
+
+class GraphicsUpgrade(decoratedLaptop: Laptop, graphicsOption: GraphicsOption )
+    : LaptopDecorator(decoratedLaptop, graphicsOption.price, graphicsOption.description, "Graphics Card")
+
+class ShinyCaseUpgrade(decoratedLaptop: Laptop, optionPrice:Int )
+    : LaptopDecorator(decoratedLaptop, optionPrice, "", "Shiny case")
+
+enum class MemoryOption (val description:String, val price:Int) {
+    _16Gb ("16Gb",50),
+    _32Gb ("32Gb", 90)
 }
-/*
-Hard Drive upgrade
 
-Graphic Card upgrade
+enum class ProcessorOption (val description:String, val price:Int) {
+    quadCore ("Quad Core, 2.3 Ghz",100),
+    quadCoreFast ("Quad Core, 2.8 Ghz",200),
+    sixCoreFast ("Six Core, 2.8 Ghz",300),
+}
 
-Battery upgrade
+enum class DriveOption (val description:String, val price:Int) {
+    _256SSD ("256 Gbyte",100),
+    _512SSD ("512 Gbyte",150),
+    _1TBSSD ("1Tbyte",200)
+}
 
-A shiny case
- */
+enum class GraphicsOption (val description:String, val price:Int) {
+    option1 ("NVIDIA® GeForce® RTX™ 2070",200),
+    option2 ("NVIDIA® GeForce® MX250",400)
+}
--- challenge22/src/test/kotlin/MainTest.kt
+++ challenge22/src/test/kotlin/MainTest.kt
@@ -3,9 +3,23 @@ import org.junit.jupiter.api.Test
 class MainTest {
     @Test
     fun `try out decorator`() {
-        val basicLaptop: Laptop = BasicLaptop()
-        val laptopWithProcessorUpgrade: Laptop = ProcessUpgrade(basicLaptop)
-        val laptopWithProcessorAndMemoryUpgrade = MemoryUpgradeDecorator(laptopWithProcessorUpgrade)
-        println(laptopWithProcessorAndMemoryUpgrade.description())
+        val basicLaptop: Laptop = BasicLaptop(400)
+        val laptopWithProcessorUpgrade: Laptop = ProcessorUpgrade(basicLaptop, ProcessorOption.quadCore)
+        val laptopWithProcessorAndMemoryUpgrade = MemoryUpgrade(laptopWithProcessorUpgrade, MemoryOption._16Gb)
+        println(laptopWithProcessorAndMemoryUpgrade)
+        println(laptopWithProcessorAndMemoryUpgrade.price)
+
+        val lapTopWithSSDUpgrade = DriveUpgrade(basicLaptop, DriveOption._512SSD)
+        println(lapTopWithSSDUpgrade)
+
+        val laptopWithGraphicsUpgrade = GraphicsUpgrade(basicLaptop, GraphicsOption.option2)
+        println(laptopWithGraphicsUpgrade)
+
+        val laptopWithShinyCaseUpgrade = ShinyCaseUpgrade(basicLaptop, 200)
+        println(laptopWithShinyCaseUpgrade)
+
+        //This will throw an error
+        val laptopWithTwoProcessUpgrades = ProcessorUpgrade(laptopWithProcessorUpgrade, ProcessorOption.sixCoreFast)
+        println(laptopWithTwoProcessUpgrades) // never reached
     }
 }
