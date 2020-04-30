d061f963292b1daf461e13f8cde68eec2e2b0030
Mike Neilens <mike.neilens@gmail.com>
2020-04-30 14:59:24 +0100

    refactor plus added a test for rotating back of cube CCW

--- challenge26/src/test/kotlin/MainTest.kt
+++ challenge26/src/test/kotlin/MainTest.kt
@@ -7,20 +7,20 @@ class MainTest {
                                 4, 5, 6,
                                 7, 8, 9)
     private val back =  listOf(10,11,12,
-                       13,14,15,
-                       16,17,18)
+                               13,14,15,
+                               16,17,18)
     private val left =  listOf(19,20,21,
-                       22,23,24,
-                       25,26,27)
+                               22,23,24,
+                               25,26,27)
     private val right = listOf(28,29,30,
-                       31,32,33,
-                       34,35,36)
+                               31,32,33,
+                               34,35,36)
     private val top =   listOf(37,38,39,
-                       40,41,42,
-                       43,44,45)
+                               40,41,42,
+                               43,44,45)
     private val bottom =listOf(46,47,48,
-                       49,50,51,
-                       52,53,54)
+                               49,50,51,
+                               52,53,54)
     private val cube = listOf(front, back, left, right, top, bottom)
 
     @Test
