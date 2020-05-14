50a414ff02b0ae3b99ee9f820aa6150f9f45de7c
Mike Neilens <mike.neilens@gmail.com>
2020-05-14 22:32:27 +0100

    working version

--- challenge27/src/main/kotlin/Main.kt
+++ challenge27/src/main/kotlin/Main.kt
@@ -5,28 +5,156 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.IOException
 import java.net.URL
 
+typealias Shot = String
+typealias Result = String
+typealias ResultMap = MutableMap<Shot, Result>
+
 val mapper: ObjectMapper = ObjectMapper().registerKotlinModule().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
 data class Data(
     val results: List<String>
 )
 
-interface Requestor {
-    fun makeRequest(parm:String="shots=E4"):List<String>
+interface Requester {
+    fun makeRequest(param:String="shots=E4"):List<Result>
 }
 
-object RequestObject:Requestor {
-    override fun makeRequest(parm:String):List<String> {
+object RequestObject:Requester {
+    override fun makeRequest(param:String):List<Result> {
         val response = try {
-            URL("https://challenge27.appspot.com/?$parm")
+            URL("https://challenge27.appspot.com/?$param")
                 .openStream()
                 .bufferedReader()
                 .use { it.readText() }
         } catch (e: IOException) {
             "Error with ${e.message}."
         }
-        var data:Data = mapper.readValue(response)
+        val data:Data = mapper.readValue(response)
         return data.results
     }
 }
 
+val rows = listOf("A","B","C","D","E","F","G","H","I","J")
+val cols = listOf("0","1","2","3","4","5","6","7","8","9")
+
+fun Int.asCoordinate():Shot = rows[this % 10] + cols[this / 10]
+fun Shot.asInt():Int {
+    val rowAsInt =  rows.indexOf(this[0].toString())
+    val colAsInt =  cols.indexOf(this[1].toString())
+    return rowAsInt + colAsInt * 10
+}
+
+val randomShots:List<Shot> = (0..99).toList().shuffled().map{it.asCoordinate()}
+
+fun fireShot(shots: List<Shot>, resultsMap: ResultMap, requester:Requester, player:String = "", game:String = ""): ResultMap {
+    val shotsJoined = shots.joinToString("")
+    var param = "shots=$shotsJoined"
+    if (player.isNotEmpty()) param = "$param&player=$player"
+    if (game.isNotEmpty()) param = "$param&game=$game"
+    val results = requester.makeRequest(param)
+    results.forEachIndexed { index, result ->
+        resultsMap[shots[index]] = result
+    }
+    return resultsMap
+}
+fun fireShotsUntilAllSunk(resultsMap: ResultMap, requester: Requester = RequestObject, noOfSquares:Int = 18, player:String = "", game:String = ""):ResultMap {
+        var index = 0
+        while (!resultsMap.allSunk(noOfSquares)) {
+            val shot = randomShots[++index]
+            if (resultsMap[shot] == null) {
+                val shots = listOf(shot)
+                fireShot(shots,resultsMap, requester, player, game)
+                if (resultsMap[shot] == "H") {
+                    fireMoreShots(Shot::shotToRightOrNull, shots, resultsMap, requester, player, game)
+                    fireMoreShots(Shot::shotToLeftOrNull, shots, resultsMap, requester, player, game)
+                    fireMoreShots(Shot::shotUpOrNull, shots, resultsMap, requester, player, game)
+                    fireMoreShots(Shot::shotDownOrNull, shots, resultsMap, requester, player, game)
+
+                    resultsMap.sinkShips()
+                    resultsMap.surroundSunkenShipsWithWater()
+                }
+            }
+        }
+        return resultsMap
+}
+
+tailrec fun fireMoreShots(getAdditionalShot:Shot.(ResultMap) -> Shot? ,shots: List<Shot>, resultsMap: ResultMap, requester:Requester, player:String = "", game:String = ""):List<Shot> {
+    if (resultsMap[shots.last()] != "H" || shots.last().getAdditionalShot(resultsMap) == null ) return shots
+
+    val additionalShot =  shots.last().getAdditionalShot(resultsMap)
+    if (additionalShot == null) return shots
+
+    fireShot(shots + additionalShot, resultsMap, requester, player, game)
+
+    return if (resultsMap.hitOrSunk(additionalShot)) fireMoreShots(getAdditionalShot,shots + additionalShot, resultsMap, requester, player, game)
+    else shots
+}
+
+fun ResultMap.sinkShips() {
+    toList().forEach{(shot, result) -> if (result == "H") this[shot] = "S"}
+}
+
+fun ResultMap.surroundSunkenShipsWithWater(){
+    toList().forEach { (shot, result) ->
+        if (result == "S") {
+            val locationToReplace = listOf(shot.toRowAbove, shot.toRowBelow, shot.toTheLeft, shot.toTheRight, shot.toLeftAndDown, shot.toLeftAndUp,shot.toRightAndDown, shot.toRightAndUp)
+            locationToReplace.forEach { replaceEmptyLocation(it)}
+        }
+    }
+}
+fun ResultMap.replaceEmptyLocation(shot:Shot?) {
+    if (shot != null && this[shot] == null) this[shot] = "m"
+}
+
+fun Shot.shotToRightOrNull(resultsMap: ResultMap):Shot? {
+    if (toTheRight != null && toTheRight?.toTheRight != null && resultsMap.hitOrSunk(toTheRight?.toTheRight!!) ) return null
+    if (toTheLeft != null && resultsMap[toTheLeft!!] == "H" && toTheRight!= null && resultsMap[toTheRight!!] == null ) return toTheRight
+    if (( (toRowAbove != null  && resultsMap[toRowAbove!!] != "H") || toRowAbove == null) && ((toRowBelow != null && resultsMap[toRowBelow!!] != "H") || toRowBelow == null)) return toTheRight
+    return null
+}
+fun Shot.shotToLeftOrNull(resultsMap: ResultMap):Shot? {
+    if (toTheLeft != null && toTheLeft?.toTheLeft != null && resultsMap.hitOrSunk(toTheLeft?.toTheLeft!!) ) return null
+    if (toTheRight != null && resultsMap[toTheRight!!] == "H" && toTheLeft!= null && resultsMap[toTheLeft!!] == null ) return toTheLeft
+    if (( (toRowAbove != null  && resultsMap[toRowAbove!!] != "H") || toRowAbove == null) && ((toRowBelow != null && resultsMap[toRowBelow!!] != "H") || toRowBelow == null)) return toTheLeft
+    return null
+}
+fun Shot.shotUpOrNull(resultsMap: ResultMap):Shot? {
+    if (toRowAbove != null && toRowAbove?.toRowAbove != null && resultsMap.hitOrSunk(toRowAbove?.toRowAbove!!) ) return null
+    if (toRowBelow != null && resultsMap[toRowBelow!!] == "H" && toRowAbove!= null && resultsMap[toRowAbove!!] == null ) return toRowAbove
+    if (( (toTheLeft != null  && resultsMap[toTheLeft!!] != "H") || toTheLeft == null ) && ((toTheRight != null && resultsMap[toTheRight!!] != "H") || toTheRight == null)) return toRowAbove
+    return null
+}
+fun Shot.shotDownOrNull(resultsMap: ResultMap):Shot? {
+    if (toRowBelow != null && toRowBelow?.toRowBelow != null && resultsMap.hitOrSunk(toRowBelow?.toRowBelow!!) ) return null
+    if (toRowAbove != null && resultsMap[toRowAbove!!] == "H" && toRowBelow!= null && resultsMap[toRowBelow!!] == null ) return toRowBelow
+    if (( (toTheLeft != null  && resultsMap[toTheLeft!!] != "H") || toTheLeft == null ) && ((toTheRight != null && resultsMap[toTheRight!!] != "H") || toTheRight == null)) return toRowBelow
+    return null
+}
+
+fun ResultMap.allSunk(noOfSquares:Int) = (values.filter{it == "S" || it == "H"}.size == noOfSquares)
+fun ResultMap.hitOrSunk(shot:Shot) = this[shot] == "H" || this[shot] == "S"
+
+val Shot.toTheRight get() = if (this != "J9" && right[1] == this[1] ) right else null
+val Shot.toTheLeft get() = if (this != "A0" && left[1] == this[1] ) left else null
+val Shot.toRowAbove get() = if (this[1] != '0' && up[0] == this[0] ) up else null
+val Shot.toRowBelow get() = if (this[1] != '9' && down[0] == this[0] ) down else null
+val Shot.toRightAndDown:Shot? get() = if (toTheRight != null) toTheRight?.toRowBelow else null
+val Shot.toLeftAndDown:Shot? get() = if (toTheLeft != null) toTheLeft?.toRowBelow else null
+val Shot.toRightAndUp:Shot? get() = if (toTheRight != null) toTheRight?.toRowAbove else null
+val Shot.toLeftAndUp:Shot? get() = if (toTheLeft != null) toTheLeft?.toRowAbove else null
+
+
+val Shot.left get() = (asInt() - 1).asCoordinate()
+val Shot.right get() = (asInt() + 1).asCoordinate()
+val Shot.up get() = (asInt() - 10).asCoordinate()
+val Shot.down get()  = (asInt() + 10).asCoordinate()
+
+fun ResultMap.print() {
+    cols.forEach{number ->
+        var data = ""
+        rows.forEach{letter ->
+            data += (this["$letter$number"] ?: ".")
+        }
+        println(data)
+    }
+}
--- challenge27/src/test/kotlin/MainTest.kt
+++ challenge27/src/test/kotlin/MainTest.kt
@@ -1,3 +1,4 @@
+
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -7,4 +8,316 @@ class MainTest {
         val result = RequestObject.makeRequest()
         assertEquals(listOf("M"), result)
     }
+
+    object mockRequestor:Requester {
+        var results:MutableMap<String, List<Result>> = mutableMapOf()
+        override fun makeRequest(param:String):List<Result> = results[param] ?: listOf()
+    }
+    @Test
+    fun `coordinate for integet 0 should be A0`() {
+        assertEquals("A0", 0.asCoordinate())
+    }
+    @Test
+    fun `coordinate for integet 9 should be J0`() {
+        assertEquals("J0", 9.asCoordinate())
+    }
+    @Test
+    fun `coordinate for integet 90 should be A9`() {
+        assertEquals("A9", 90.asCoordinate())
+    }
+    @Test
+    fun `coordinate for integet 99 should be J9`() {
+        assertEquals("J9", 99.asCoordinate())
+    }
+
+    @Test
+    fun `integer for coordinate A0 should be 0`() {
+        assertEquals(0, "A0".asInt())
+    }
+    @Test
+    fun `integer for coordinate J0 should be 9`() {
+        assertEquals(9, "J0".asInt())
+    }
+    @Test
+    fun `integer for coordinate A9 should be 90`() {
+        assertEquals(90, "A9".asInt())
+    }
+    @Test
+    fun `integer for coordinate J9 should be 99`() {
+        assertEquals(99, "J9".asInt())
+    }
+
+    @Test
+    fun `firing a shot that results in a miss puts a miss on the results map`() {
+        val resultsMap:ResultMap = mutableMapOf()
+        mockRequestor.results = mutableMapOf("shots=A0" to listOf("M"))
+        val result = fireShot(listOf("A0"),resultsMap, mockRequestor)
+        assertEquals("M", result["A0"])
+    }
+    @Test
+    fun `firing a shot that results in a hit puts a hit on the results map`() {
+        val resultsMap:ResultMap = mutableMapOf()
+        mockRequestor.results = mutableMapOf("shots=B4" to listOf("H"))
+        val result = fireShot(listOf("B4"),resultsMap, mockRequestor)
+        assertEquals("H", result["B4"])
+    }
+    @Test
+    fun `firing two shots that results in a hit and miss puts a hit and miss on the results map`() {
+        val resultsMap:ResultMap = mutableMapOf()
+        mockRequestor.results = mutableMapOf("shots=B4A0" to listOf("H","M"))
+        val result = fireShot(listOf("B4","A0"),resultsMap, mockRequestor)
+        assertEquals("H", result["B4"])
+        assertEquals("M", result["A0"])
+    }
+    @Test
+    fun `firing shots until all ships are sunk returns a map with all ships sunk`() {
+        val resultsMap:ResultMap = mutableMapOf()
+        mockRequestor.results =  (0..99).toList().shuffled().map{"shots=" + it.asCoordinate() to kotlin.collections.listOf("M") }.toMap().toMutableMap()
+        mockRequestor.results["shots=D4"] = listOf("H")
+        mockRequestor.results["shots=D5"] = listOf("H")
+        mockRequestor.results["shots=D6"] = listOf("H")
+        val result = fireShotsUntilAllSunk(resultsMap, mockRequestor, 3)
+        assertEquals(3, result.values.filter{it == "H"}.size)
+    }
+
+    @Test
+    fun `firing more shots to the right when last shot hit and the square to the right is a ship results in square to the right being hit`() {
+        val resultsMap:ResultMap = mutableMapOf("D4" to "H")
+        val shots = listOf("D4")
+        mockRequestor.results["shots=D4E4"] = listOf("H","H")
+        val result = fireMoreShots(Shot::shotToRightOrNull, shots, resultsMap, mockRequestor)
+        assertEquals("H",resultsMap["D4"])
+        assertEquals("H",resultsMap["E4"])
+    }
+
+    @Test
+    fun `firing more shots to the right when last shot hit and the two squares to the right are ships results in both squares to the right being hit`() {
+        val resultsMap:ResultMap = mutableMapOf("D4" to "H")
+        val shots = listOf("D4")
+        mockRequestor.results["shots=D4E4"] = listOf("H","H")
+        mockRequestor.results["shots=D4E4F4"] = listOf("H","H","H")
+        val result = fireMoreShots(Shot::shotToRightOrNull, shots, resultsMap, mockRequestor)
+        assertEquals("H",resultsMap["D4"])
+        assertEquals("H",resultsMap["E4"])
+        assertEquals("H",resultsMap["F4"])
+    }
+    @Test
+    fun `firing more shots to the right when last shot hit and the two squares to the right are ship and sea results in one squares to the right being hit`() {
+        val resultsMap:ResultMap = mutableMapOf("D4" to "H")
+        val shots = listOf("D4")
+        mockRequestor.results["shots=D4E4"] = listOf("H","H")
+        mockRequestor.results["shots=D4E4F4"] = listOf("H","H","M")
+        val result = fireMoreShots(Shot::shotToRightOrNull, shots, resultsMap, mockRequestor)
+        assertEquals("H",resultsMap["D4"])
+        assertEquals("H",resultsMap["E4"])
+        assertEquals("M",resultsMap["F4"])
+    }
+
+    @Test
+    fun `firing more shots to the left when last shot hit and the square to the left is a ship results in square to the left being hit`() {
+        val resultsMap:ResultMap = mutableMapOf("D4" to "H")
+        val shots = listOf("D4")
+        mockRequestor.results["shots=D4C4"] = listOf("H","H")
+        val result = fireMoreShots(Shot::shotToLeftOrNull, shots, resultsMap, mockRequestor)
+        assertEquals("H",resultsMap["D4"])
+        assertEquals("H",resultsMap["C4"])
+        assertEquals(listOf("D4","C4"), result)
+    }
+
+    @Test
+    fun `firing more shots to the left when last shot hit and the two squares to the left are ships results in both squares to the left being hit`() {
+        val resultsMap:ResultMap = mutableMapOf("D4" to "H")
+        val shots = listOf("D4")
+        mockRequestor.results["shots=D4C4"] = listOf("H","H")
+        mockRequestor.results["shots=D4C4B4"] = listOf("H","H","H")
+        val result = fireMoreShots(Shot::shotToLeftOrNull, shots, resultsMap, mockRequestor)
+        assertEquals("H",resultsMap["D4"])
+        assertEquals("H",resultsMap["C4"])
+        assertEquals("H",resultsMap["B4"])
+        assertEquals(listOf("D4","C4","B4"), result)
+    }
+    @Test
+    fun `firing more shots to the left when last shot hit and the two squares to the left are ship and sea results in one squares to the left being hit`() {
+        val resultsMap:ResultMap = mutableMapOf("D4" to "H")
+        val shots = listOf("D4")
+        mockRequestor.results["shots=D4C4"] = listOf("H","H")
+        mockRequestor.results["shots=D4C4B4"] = listOf("H","H","M")
+        val result = fireMoreShots(Shot::shotToLeftOrNull, shots, resultsMap, mockRequestor)
+        assertEquals("H",resultsMap["D4"])
+        assertEquals("H",resultsMap["C4"])
+        assertEquals("M",resultsMap["B4"])
+        assertEquals(listOf("D4","C4"), result)
+    }
+    @Test
+    fun `firing more shots up when last shot hit and the three squares above contain ships`() {
+        val resultsMap:ResultMap = mutableMapOf("B5" to "H")
+        val shots = listOf("B5")
+        mockRequestor.results["shots=B5B4"] = listOf("H","H")
+        mockRequestor.results["shots=B5B4B3"] = listOf("H","H","H")
+        mockRequestor.results["shots=B5B4B3B2"] = listOf("H","H","H","H")
+        val result = fireMoreShots(Shot::shotUpOrNull, shots, resultsMap, mockRequestor)
+        assertEquals("H",resultsMap["B5"])
+        assertEquals("H",resultsMap["B4"])
+        assertEquals("H",resultsMap["B3"])
+        assertEquals("H",resultsMap["B2"])
+    }
+    @Test
+    fun `firing more shots down when last shot hit and the three squares below contain ships`() {
+        val resultsMap:ResultMap = mutableMapOf("B5" to "H")
+        val shots = listOf("B5")
+        mockRequestor.results["shots=B5B6"] = listOf("H","H")
+        mockRequestor.results["shots=B5B6B7"] = listOf("H","H","H")
+        mockRequestor.results["shots=B5B6B7B8"] = listOf("H","H","H","H")
+        val result = fireMoreShots(Shot::shotDownOrNull, shots, resultsMap, mockRequestor)
+        assertEquals("H",resultsMap["B5"])
+        assertEquals("H",resultsMap["B6"])
+        assertEquals("H",resultsMap["B7"])
+        assertEquals("H",resultsMap["B8"])
+    }
+    @Test
+    fun `shot to right or null gives null if the square two places to the right of the current position contains a ship `() {
+        val resultsMap:ResultMap = mutableMapOf("E5" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotToRightOrNull(resultsMap))
+    }
+    @Test
+    fun `shot to right or null gives shot to right of current shot if the square the left of the current position contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("B5" to "H")
+        val shot = "C5"
+        assertEquals("D5", shot.shotToRightOrNull(resultsMap))
+    }
+    @Test
+    fun `shot to right or null gives shot to right of current shot if the square the left of the current position is empty and positions above and below are empty `() {
+        val resultsMap:ResultMap = mutableMapOf()
+        val shot = "C5"
+        assertEquals("D5", shot.shotToRightOrNull(resultsMap))
+    }
+    @Test
+    fun `shot to right or null gives null if the square the left of the current position is empty and positions above contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("C4" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotToRightOrNull(resultsMap))
+    }
+    @Test
+    fun `shot to right or null gives null if the square the left of the current position is empty and positions below contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("C6" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotToRightOrNull(resultsMap))
+    }
+
+    //
+    @Test
+    fun `shot to left or null gives null if the square two places to the left of the current position contains a ship `() {
+        val resultsMap:ResultMap = mutableMapOf("A5" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotToLeftOrNull(resultsMap))
+    }
+    @Test
+    fun `shot to left or null gives shot to left of current shot if the square the right of the current position contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("D5" to "H")
+        val shot = "C5"
+        assertEquals("B5", shot.shotToLeftOrNull(resultsMap))
+    }
+    @Test
+    fun `shot to left or null gives shot to left of current shot if the square the right of the current position is empty and positions above and below are empty `() {
+        val resultsMap:ResultMap = mutableMapOf()
+        val shot = "C5"
+        assertEquals("B5", shot.shotToLeftOrNull(resultsMap))
+    }
+    @Test
+    fun `shot to left or null gives null if the square the right of the current position is empty and positions above contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("C4" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotToLeftOrNull(resultsMap))
+    }
+    @Test
+    fun `shot to left or null gives null if the square the right of the current position is empty and positions below contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("C6" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotToLeftOrNull(resultsMap))
+    }
+
+    @Test
+    fun `shot Up or null gives null if the square two places above the current position contains a ship `() {
+        val resultsMap:ResultMap = mutableMapOf("C3" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotUpOrNull(resultsMap))
+    }
+    @Test
+    fun `shot Up or null gives shot above current shot if the square below the current position contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("C6" to "H")
+        val shot = "C5"
+        assertEquals("C4", shot.shotUpOrNull(resultsMap))
+    }
+    @Test
+    fun `shot Up or null gives shot above current shot if the square below current position is empty and positions left and right of current shot are empty `() {
+        val resultsMap:ResultMap = mutableMapOf()
+        val shot = "C5"
+        assertEquals("C4", shot.shotUpOrNull(resultsMap))
+    }
+    @Test
+    fun `shot Up or null gives null if the square below current position is empty and position to right contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("D5" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotUpOrNull(resultsMap))
+    }
+    @Test
+    fun `shot Up or null gives null if below the current position is empty and positions to left contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("B5" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotUpOrNull(resultsMap))
+    }
+
+    @Test
+    fun `shot Down or null gives null if the square two places below the current position contains a ship `() {
+        val resultsMap:ResultMap = mutableMapOf("C7" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotDownOrNull(resultsMap))
+    }
+    @Test
+    fun `shot Down or null gives shot below current shot if the square above the current position contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("C4" to "H")
+        val shot = "C5"
+        assertEquals("C6", shot.shotDownOrNull(resultsMap))
+    }
+    @Test
+    fun `shot Up or null gives shot below current shot if the square above current position is empty and positions left and right of current shot are empty `() {
+        val resultsMap:ResultMap = mutableMapOf()
+        val shot = "C5"
+        assertEquals("C6", shot.shotDownOrNull(resultsMap))
+    }
+    @Test
+    fun `shot Down or null gives null if the square above current position is empty and position to right contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("D5" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotDownOrNull(resultsMap))
+    }
+    @Test
+    fun `shot Down or null gives null if below the current position is empty and positions to left contains a hit `() {
+        val resultsMap:ResultMap = mutableMapOf("B5" to "H")
+        val shot = "C5"
+        assertEquals(null, shot.shotDownOrNull(resultsMap))
+    }
+
+    @Test
+    fun `firing shots until all ships are sunk returns a map with all ships sunk when using real service`() {
+        val resultsMap:ResultMap = mutableMapOf()
+        val result = fireShotsUntilAllSunk(resultsMap)
+        assertEquals(18, result.values.filter{it == "H" || it == "S"}.size)
+    }
+
+    @Test
+    fun `firing shots until all ships are sunk returns a map with all ships sunk when using real service with game1`() {
+        val resultsMap:ResultMap = mutableMapOf()
+        val result = fireShotsUntilAllSunk(resultsMap, game = "game1")
+        assertEquals(18, result.values.filter{it == "H" || it == "S"}.size)
+    }
+
+    @Test
+    fun `firing shots until all ships are sunk returns a map with all ships sunk when using real service with game2`() {
+        val resultsMap:ResultMap = mutableMapOf()
+        val result = fireShotsUntilAllSunk(resultsMap, game = "game2")
+        assertEquals(18, result.values.filter{it == "H" || it == "S"}.size)
+    }
+
 }
