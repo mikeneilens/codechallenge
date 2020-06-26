51053dbe5d2364e19c05df9ef3b8aea9bbd8b5cc
Mike Neilens <mike.neilens@gmail.com>
2020-06-26 17:10:24 +0100

    added merge sort

--- challenge29/src/main/kotlin/Main.kt
+++ challenge29/src/main/kotlin/Main.kt
@@ -10,10 +10,44 @@ fun binarySearch(list: List<Int>, value:Int): Boolean {
     if (list.isEmpty()) return false
     if (list.size == 1) return (list.first() == value)
     if (value == list.midValue ) return true
-    if (value  < list.midValue ) return binarySearch(list.firstHalf(), value)
-    else return  binarySearch(list.secondtHalf(), value)
+    return if (value  < list.midValue ) binarySearch(list.firstHalf(), value)
+    else binarySearch(list.secondHalf(), value)
 }
 val List<Int>.midValue:Int  get() = get(size/2)
 fun List<Int>.firstHalf():List<Int> = dropLast(size - size / 2)
-fun List<Int>.secondtHalf():List<Int> = drop(size / 2)
+fun List<Int>.secondHalf():List<Int> = drop(size / 2)
 
+tailrec fun insertionSort(list: List<Int>, result:List<Int> = emptyList()): List<Int> {
+    val itemsToAdd = list.findAllMinimums(result)
+    if (itemsToAdd.isEmpty()) return result
+    return insertionSort(list, result + itemsToAdd )
+}
+
+fun List<Int>.findAllMinimums(newList:List<Int> = emptyList()):List<Int> {
+    var minimum:Int =  Int.MAX_VALUE
+    var result = mutableListOf<Int>()
+    forEach { value ->
+        if (value == minimum) result.add((value))
+        else if ((value < minimum) && (newList.isEmpty() || value > newList.last())) {
+            minimum = value
+            result = mutableListOf(value)
+        }
+    }
+    return result
+}
+
+fun mergeSort(list:List<Int>):List<Int> {
+    if (list.size < 2) return list
+    return merge( mergeSort(list.firstHalf()), mergeSort(list.secondHalf()))
+}
+
+fun merge(list1:List<Int>, list2:List<Int>, result:List<Int> = listOf()) : List<Int> {
+    if (list1.isEmpty() && list2.isEmpty()) return result
+    if (list1.isEmpty()) return result + list2
+    if (list2.isEmpty()) return result + list1
+    return if (list1.first() < list2.first()) {
+        merge(list1.drop(1), list2, result + list1.first())
+    } else {
+        merge(list1, list2.drop(1), result + list2.first())
+    }
+}
--- challenge29/src/test/kotlin/MainTest.kt
+++ challenge29/src/test/kotlin/MainTest.kt
@@ -1,5 +1,4 @@
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 val testList = listOf(914,161,396,490,978,831,534,813,644,642,312,143,688,637,333,89,250,426,290,39,330,826,3,63,598,751,700,19,104,794,729,212,565,490,903,55,97,81,260,4,458,729,90,673,75,476,697,448,411,976,527,87,496,929,955,800,786,411,263,5,852,806,51,537,804,833,627,357,731,259,702,942,942,224,79,385,220,771,878,894,824,82,225,843,540,131,300,712,615,937,69,271,28,400,29,161,194,704,820,555)
@@ -46,6 +45,112 @@ class MainTest {
     fun `binary searching a list returns true if the list contain the item at the middle`() {
         val list = listOf(1,2,3,4,5,6)
         assertTrue(binarySearch(list, 4))
+    }
+
+    @Test
+    fun `findMinimum obtains the values in the list with the smallest value`() {
+        val list = listOf(4,8,3,6,3,8,2,8,2,9,5)
+        assertEquals(listOf(2,2), list.findAllMinimums())
+    }
+
+
+    @Test
+    fun `insertion sort returns an empty list if sorting an empty list`() {
+        val list = emptyList<Int>()
+        assertEquals(emptyList<Int>(), insertionSort(list))
+
+    }
+    @Test
+    fun `insertion sort returns a list of one if sorting a list of one`() {
+        val list = listOf(1)
+        assertEquals(listOf(1), insertionSort(list))
+
+    }
+    @Test
+    fun `insertion sort returns a list of two if sorting a list of two that is already in the correct order`() {
+        val list = listOf(3,4)
+        assertEquals(listOf(3,4), insertionSort(list))
+
+    }
+    @Test
+    fun `insertion sort returns a list of two if sorting a list of two that is not already in the correct order`() {
+        val list = listOf(4,3)
+        assertEquals(listOf(3,4), insertionSort(list))
+
+    }
+    @Test
+    fun `insertion sort returns a list of four if sorting a list of four that is not already in the correct order`() {
+        val list = listOf(4,3,4,1)
+        assertEquals(listOf(1,3,4,4), insertionSort(list))
+
+    }
+    @Test
+    fun `insertion sort returns a long list of items sorted correctly`() {
+        val expectedResult = testList.sorted()
+        assertEquals(expectedResult, insertionSort(testList))
+
+    }
+
+    @Test
+    fun `merging two lists with both empty gives an empty list`() {
+        val list1 = emptyList<Int>()
+        val list2 = emptyList<Int>()
+        assertEquals(emptyList<Int>(), merge(list1, list2))
+    }
+    @Test
+    fun `merging two lists with first empty gives second list, plus the result`() {
+        val list1 = emptyList<Int>()
+        val list2 = listOf(3,4,5)
+        assertEquals(listOf(1,2,3,4,5), merge(list1, list2, listOf(1,2)))
+    }
+    @Test
+    fun `merging two lists with second empty gives first list, plus the result`() {
+        val list1 = listOf(3,4,5)
+        val list2 = emptyList<Int>()
+        assertEquals(listOf(1,2,3,4,5), merge(list1, list2, listOf(1,2)))
+    }
+    @Test
+    fun `merging two lists each containing sorted lists creates a new sorted list`() {
+        val list1 = listOf(3,4,5,9,15)
+        val list2 = listOf(1,4,6,8,9,16)
+        assertEquals(listOf(1,3,4,4,5,6,8,9,9,15,16), merge(list1, list2))
+    }
+
+    @Test
+    fun `merge sort returns an empty list if sorting an empty list`() {
+        val list = emptyList<Int>()
+        assertEquals(emptyList<Int>(), mergeSort(list))
+
+    }
+    @Test
+    fun `merge sort returns a list of one if sorting a list of one`() {
+        val list = listOf(1)
+        assertEquals(listOf(1), mergeSort(list))
+
+    }
+    @Test
+    fun `merge sort returns a list of two if sorting a list of two that is already in the correct order`() {
+        val list = listOf(3,4)
+        assertEquals(listOf(3,4), mergeSort(list))
+
+    }
+    @Test
+    fun `merge sort returns a list of two if sorting a list of two that is not already in the correct order`() {
+        val list = listOf(4,3)
+        assertEquals(listOf(3,4), mergeSort(list))
+
+    }
+    @Test
+    fun `merge sort returns a list of four if sorting a list of four that is not already in the correct order`() {
+        val list = listOf(4,3,4,1)
+        assertEquals(listOf(1,3,4,4), mergeSort(list))
+
+    }
+    @Test
+    fun `merge sort returns a long list of items sorted correctly`() {
+        val expectedResult = testList.sorted()
+        assertEquals(expectedResult, mergeSort(testList))
+
     }
 
 }
