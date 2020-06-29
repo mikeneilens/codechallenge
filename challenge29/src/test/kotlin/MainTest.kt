0d4a82d84918fa2616233049bc393111185a90eb
Mike Neilens <mike.neilens@gmail.com>
2020-06-29 17:54:03 +0100

    added better tests

--- challenge29/src/test/kotlin/MainTest.kt
+++ challenge29/src/test/kotlin/MainTest.kt
@@ -1,11 +1,16 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-
-val testList = listOf(914,161,396,490,978,831,534,813,644,642,312,143,688,637,333,89,250,426,290,39,330,826,3,63,598,751,700,19,104,794,729,212,565,490,903,55,97,81,260,4,458,729,90,673,75,476,697,448,411,976,527,87,496,929,955,800,786,411,263,5,852,806,51,537,804,833,627,357,731,259,702,942,942,224,79,385,220,771,878,894,824,82,225,843,540,131,300,712,615,161,69,271,28,400,29,161,194,704,820,555)
+import kotlin.random.Random
 
 class MainTest {
-    val testData = (testList + testList + testList + testList + testList + testList + testList + testList + testList + testList).shuffled()
+    val testData = List(1000) { Random.nextInt(0, 1000) }
     val sortedTestData = testData.sorted()
+
+    val testData10000 = List(10000) { Random.nextInt(0, 10000) }
+    val sortedTestData10000 = testData10000.sorted()
+
+    val testData100000 =  List(100000) { Random.nextInt(0, 10000) }
+    val sortedTestData100000 = testData100000.sorted()
 
     @Test
     fun `naive searching a list returns false if list is empty`() {
@@ -90,8 +95,14 @@ class MainTest {
     fun `insertion sort returns a long list of items sorted correctly`() {
         println("Starting insertion sort")
         val startTime = System.currentTimeMillis()
-        val expectedResult = testList.sorted()
-        assertEquals(expectedResult, insertionSort(testList))
+        assertEquals(sortedTestData, insertionSort(testData))
+        println("Approx elapsed ${System.currentTimeMillis() - startTime} ")
+    }
+    @Test
+    fun `insertion sort with loop returns a longer list of items sorted correctly`() {
+        println("Starting insertion sort with loop, 10000 items")
+        val startTime = System.currentTimeMillis()
+        assertEquals(sortedTestData10000, insertionSort(testData10000))
         println("Approx elapsed ${System.currentTimeMillis() - startTime} ")
     }
 
@@ -150,28 +161,11 @@ class MainTest {
         assertEquals(listOf(1,3,4,4), mergeSort(list))
 
     }
-
     @Test
     fun `merge sort returns a long list of items sorted correctly`() {
         println("Starting merge sort")
         val startTime = System.currentTimeMillis()
         assertEquals(sortedTestData, mergeSort(testData))
-        println("Approx elapsed ${System.currentTimeMillis() - startTime} ")
-    }
-
-    @Test
-    fun `merge sort aysynch returns a long list of items sorted correctly`() {
-        println("Starting merge sort using coroutine")
-        val startTime = System.currentTimeMillis()
-        assertEquals(sortedTestData, mergeSortConcurrent(testData))
-        println("Approx elapsed ${System.currentTimeMillis() - startTime} ")
-    }
-
-    @Test
-    fun `merge sort not conccurrent returns a long list of items sorted correctly`() {
-        println("Starting merge sort using coroutine but not processing each branch of the sort concurrently")
-        val startTime = System.currentTimeMillis()
-        assertEquals(sortedTestData, mergeSortNotConcurrent(testData))
         println("Approx elapsed ${System.currentTimeMillis() - startTime} ")
     }
 
@@ -203,14 +197,39 @@ class MainTest {
     fun `merge sort with loop returns a list of four if sorting a list of four that is not already in the correct order`() {
         val list = listOf(4,3,4,1)
         assertEquals(listOf(1,3,4,4), mergeSortLoop(list))
-
     }
+
     @Test
-    fun `merge sort with loop returns a long list of items sorted correctly`() {
-        println("Starting merge sort with loop")
-        val startTime = System.currentTimeMillis()
+    fun `compare different types of merge sorts 1000 items`() {
+        println("testing with 1000 items.....")
+        val startTimeSimple = System.currentTimeMillis()
+        assertEquals(sortedTestData, mergeSort(testData))
+        println("Simple merge sort elapsed: ${System.currentTimeMillis() - startTimeSimple} ")
+        val startTimeAsynch = System.currentTimeMillis()
+        assertEquals(sortedTestData, mergeSortConcurrent(testData))
+        println("Concurrent merge sort elapsed: ${System.currentTimeMillis() - startTimeAsynch} ")
+        val startTimeSynch = System.currentTimeMillis()
+        assertEquals(sortedTestData, mergeSortConcurrent(testData))
+        println("Corountine not concurrent merge sort elapsed: ${System.currentTimeMillis() - startTimeSynch} ")
+        val startTimeLoop = System.currentTimeMillis()
         assertEquals(sortedTestData, mergeSortLoop(testData))
-        println("Approx elapsed ${System.currentTimeMillis() - startTime} ")
+        println("Loop merge sort elapsed: ${System.currentTimeMillis() - startTimeLoop} ")
     }
 
+    @Test
+    fun `compare different types of merge sorts 10000 items`() {
+        println("testing with 10000 items.....")
+        val startTimeSimple = System.currentTimeMillis()
+        assertEquals(sortedTestData10000, mergeSort(testData10000))
+        println("Simple merge sort elapsed: ${System.currentTimeMillis() - startTimeSimple} ")
+        val startTimeAsynch = System.currentTimeMillis()
+        assertEquals(sortedTestData10000, mergeSortConcurrent(testData10000))
+        println("Concurrent merge sort elapsed: ${System.currentTimeMillis() - startTimeAsynch} ")
+        val startTimeSynch = System.currentTimeMillis()
+        assertEquals(sortedTestData10000, mergeSortConcurrent(testData10000))
+        println("Corountine not concurrent merge sort elapsed: ${System.currentTimeMillis() - startTimeSynch} ")
+        val startTimeLoop = System.currentTimeMillis()
+        assertEquals(sortedTestData10000, mergeSortLoop(testData10000))
+        println("Loop merge sort elapsed: ${System.currentTimeMillis() - startTimeLoop} ")
+    }
 }
