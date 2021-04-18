24a01ef14897af7c3e3664c49cde7e554a3ec0b9
Mike Neilens <mike.neilens@gmail.com>
2021-04-18 11:35:15 +0100

    initial code with one test

--- challenge32/src/test/kotlin/MainTest.kt
+++ challenge32/src/test/kotlin/MainTest.kt
@@ -5,12 +5,6 @@ import java.time.LocalDate
 
 class MainTest {
     @Test
-    fun `hello world`() {
-        val result = "hello world"
-        assertEquals("hello world", result)
-    }
-
-    @Test
     fun `standby claim for 1 day on a week day which is not a holiday is weekday rate`() {
         val result = calcStandbyClaim(LocalDate.parse("2021-04-19"),1,"A")
         val expectedResult = Claim(WEEKDAY_RATE_A,listOf("2021-04-19, Week day rate A, £25"))
