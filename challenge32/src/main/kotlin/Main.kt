9a98e01e7edf7f0c66514070eb954e041cc4b845
Mike Neilens <mike.neilens@gmail.com>
2021-04-18 13:42:56 +0100

    refactored to determine type of day using separate functions

--- challenge32/src/main/kotlin/Main.kt
+++ challenge32/src/main/kotlin/Main.kt
@@ -4,11 +4,12 @@ import java.time.LocalDate
 data class Claim( val amount:Int, val claimDetails:List<String>)
 
 fun calcStandbyClaim(date: LocalDate, duration: Int, calloutLevel:String): Claim {
-    if (date.isBankHoliday()) return Claim(rates.claim(ShiftType.BANK_HOLIDAY,"A") * 2,listOf("2021-04-02, ${rates.description(ShiftType.BANK_HOLIDAY,"A")}","2021-04-02, ${rates.description(ShiftType.BANK_HOLIDAY,"A")}"))
-    if (date.dayOfWeek == DayOfWeek.FRIDAY) return Claim(rates.claim(ShiftType.WEEKEND,"A") ,listOf("2021-04-16, ${rates.description(ShiftType.WEEKEND,"A")}"))
-    if (date.dayOfWeek == DayOfWeek.SATURDAY) return Claim(rates.claim(ShiftType.WEEKEND,"A") * 2,listOf("2021-04-17, ${rates.description(ShiftType.WEEKEND,"A")}","2021-04-17, ${rates.description(ShiftType.WEEKEND,"A")}"))
-    if (date.dayOfWeek == DayOfWeek.SUNDAY) return Claim(rates.claim(ShiftType.WEEKEND,"A") * 2,listOf("2021-04-18, ${rates.description(ShiftType.WEEKEND,"A")}","2021-04-18, ${rates.description(ShiftType.WEEKEND,"A")}"))
-    return Claim(rates.claim(ShiftType.MON_TO_THU,"A") ,listOf("2021-04-19, ${rates.description(ShiftType.MON_TO_THU,"A")}"))
+    if (date.isBankHoliday()) return Claim(rates.claim(ShiftType.BANK_HOLIDAY,"A") * 2,listOf("$date, ${rates.description(ShiftType.BANK_HOLIDAY,"A")}","$date, ${rates.description(ShiftType.BANK_HOLIDAY,"A")}"))
+    if (date.isFriday()) return Claim(rates.claim(ShiftType.WEEKEND,"A") ,listOf("$date, ${rates.description(ShiftType.WEEKEND,"A")}"))
+    if (date.isWeekend())  return Claim(rates.claim(ShiftType.WEEKEND,"A") * 2,listOf("$date, ${rates.description(ShiftType.WEEKEND,"A")}","$date, ${rates.description(ShiftType.WEEKEND,"A")}"))
+    return Claim(rates.claim(ShiftType.MON_TO_THU,"A") ,listOf("$date, ${rates.description(ShiftType.MON_TO_THU,"A")}"))
 }
 
+fun LocalDate.isFriday() = dayOfWeek == DayOfWeek.FRIDAY
+fun LocalDate.isWeekend() = dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY
 fun LocalDate.isBankHoliday() = this == LocalDate.parse("2021-04-02")
