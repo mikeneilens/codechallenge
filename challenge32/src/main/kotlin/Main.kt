7f44f6d93c202ca06ba81008b397e3748c073374
Mike Neilens <mike.neilens@gmail.com>
2021-04-19 22:23:51 +0100

    refactoring

--- challenge32/src/main/kotlin/Main.kt
+++ challenge32/src/main/kotlin/Main.kt
@@ -12,13 +12,13 @@ fun calcStandbyClaim(date: LocalDate, duration: Double, callOutLevel:String): Cl
 
     var amount = 0
     val details = mutableListOf<String>()
-    var offset = 0.0
+    var noOfDays = 0.0
 
-    while (offset < duration) {
-        val dateOfShift = date.plusDays(offset.toLong())
+    while (noOfDays < duration) {
+        val dateOfShift = date.plusDays(noOfDays.toLong())
         amount += rates.amount(dateOfShift.shiftType, callOutLevel)
         details.add(rates.detail(dateOfShift.shiftType, callOutLevel, dateOfShift))
-        offset += 1.0 / dateOfShift.noOfShifts
+        noOfDays += 1.0 / dateOfShift.noOfShifts
     }
 
     return Claim(amount, details)
@@ -37,45 +37,34 @@ fun LocalDate.isFriday() = dayOfWeek == DayOfWeek.FRIDAY
 fun LocalDate.isWeekend() = dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY
 
 val holidayValidators = listOf(
-    ::isChristmasDayHoliday
-    ,::isBoxingDayHoliday
-    ,::isNewYearsDayHoliday
-    ,::isGoodFridayHoliday
-    ,::isEasterMondayHoliday
-    ,::isMayDayHoliday
-    ,::isSpringHoliday
-    ,::isSummerHoliday
+    ::isNewYearsDayHoliday,
+    ::isGoodFridayHoliday,
+    ::isEasterMondayHoliday,
+    ::isMayDayHoliday,
+    ::isSpringHoliday,
+    ::isSummerHoliday,
+    ::isChristmasDayHoliday,
+    ::isBoxingDayHoliday,
 )
 
 fun LocalDate.isBankHoliday():Boolean = holidayValidators.any{it(this)}
 
-fun isChristmasDayHoliday(date: LocalDate) = (date == getChristmasDay(date.year))
+fun isChristmasDayHoliday(date: LocalDate) = (date == getChristmasDayHoliday(date.year))
 
-fun getChristmasDay(year:Int):LocalDate {
-    val december25 = LocalDate.parse("$year-12-25")
-    if (december25.dayOfWeek == DayOfWeek.SATURDAY) return december25.plusDays(2)
-    if (december25.dayOfWeek == DayOfWeek.SUNDAY) return december25.plusDays(1)
-    return december25
-}
+fun getChristmasDayHoliday(year:Int):LocalDate = weekdayOnOrAfter(LocalDate.parse("$year-12-25"))
 
-fun isBoxingDayHoliday(date: LocalDate) = (date == getBoxingDay(date.year))
+fun weekdayOnOrAfter(date:LocalDate) =
+    if (date.dayOfWeek == DayOfWeek.SATURDAY) date.plusDays(2)
+    else if (date.dayOfWeek == DayOfWeek.SUNDAY) date.plusDays(1)
+    else date
 
-fun getBoxingDay(year:Int):LocalDate {
-    val december26 = LocalDate.parse("$year-12-26")
-    if (december26.dayOfWeek == DayOfWeek.SATURDAY) return december26.plusDays(2)
-    if (december26.dayOfWeek == DayOfWeek.SUNDAY) return december26.plusDays(2)
-    if (december26.dayOfWeek == DayOfWeek.MONDAY) return december26.plusDays(1)
-    return december26
-}
+fun isBoxingDayHoliday(date: LocalDate) = (date == getBoxingDayHoliday(date.year))
 
-fun isNewYearsDayHoliday(date: LocalDate) = (date == getNewYearsDay(date.year))
+fun getBoxingDayHoliday(year:Int):LocalDate =  weekdayOnOrAfter(getChristmasDayHoliday(year).plusDays(1))
 
-fun getNewYearsDay(year:Int):LocalDate {
-    val january01 = LocalDate.parse("$year-01-01")
-    if (january01.dayOfWeek == DayOfWeek.SATURDAY) return january01.plusDays(2)
-    if (january01.dayOfWeek == DayOfWeek.SUNDAY) return january01.plusDays(1)
-    return january01
-}
+fun isNewYearsDayHoliday(date: LocalDate) = (date == getNewYearsDayHoliday(date.year))
+
+fun getNewYearsDayHoliday(year:Int):LocalDate = weekdayOnOrAfter(LocalDate.parse("$year-01-01"))
 
 fun isMayDayHoliday(date: LocalDate) =
     date.month == Month.MAY && date.dayOfWeek == DayOfWeek.MONDAY && date.dayOfMonth < 8
