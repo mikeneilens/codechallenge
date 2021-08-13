5908ce72ce3cd55b5c6dbeecd4e1f665598cab7e
Mike Neilens <mike.neilens@gmail.com>
2021-08-13 11:57:25 +0100

    added a configuration builder

--- challenge33/src/main/kotlin/Configuration.kt
+++ challenge33/src/main/kotlin/Configuration.kt
@@ -7,18 +7,4 @@ class Configuration {
     operator fun get(id:Id) = items.getValue(id)
     override fun toString() = items.values.joinToString("\n") { it.toString() }
 
-    /*
-    companion object {
-        private val items:MutableMap<Id,Item> = mutableMapOf()
-        fun addItem(item:Item) {
-            items[item.id] = item
-        }
-        operator fun get(id:Id) = items.getValue(id)
-        override fun toString() = items.values.joinToString("\n") { it.toString() }
-
-        fun reset() {
-            items.clear()
-        }
-    }
-     */
 }
