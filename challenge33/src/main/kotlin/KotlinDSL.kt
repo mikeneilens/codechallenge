ebd513276c3a83bc567cd0909c1f930a195d910c
Mike Neilens <mike.neilens@gmail.com>
2021-08-15 13:50:47 +0100

    added a Kotlin style dsl

---
+++ challenge33/src/main/kotlin/KotlinDSL.kt
@@ -0,0 +1,72 @@
+
+fun kotlinDSL(): Configuration =
+    ConfigBuilder.build {
+
+        addItem("secure_air_vent")
+        addItem("acid_bath") {
+            whichUsesAcid(){
+                withType("hcl")
+                withGrade(5)
+            }
+            whichUses(Electricity(power = 12))
+        }
+        addItem("camera") {
+            whichUses(Electricity(power = 1))
+        }
+        addItem("small_power_plant") {
+            whichProvides(Electricity(power = 11))
+            whichDependsOn("secure_air_vent")
+        }
+    }.config
+
+
+class ConfigBuilder {
+    var config = Configuration()
+
+    fun addItem(id:Id) {
+        val item = Item(id)
+        config.addItem(item)
+    }
+    fun addItem(id:Id, init:ItemBuilder2.()->Unit) {
+        val item = Item(id)
+        config.addItem(item)
+        ItemBuilder2(item).apply(init)
+    }
+
+    companion object {
+        fun build(init:ConfigBuilder.()->Unit): ConfigBuilder {
+            val configBuilder = ConfigBuilder()
+            init(configBuilder)
+            return configBuilder
+        }
+
+    }
+}
+
+class ItemBuilder2(private val item:Item) {
+    fun whichUses(anItem:Item) {
+        this.item.addUsage(anItem)
+    }
+    fun whichDependsOn(id:Id) {
+        val anItem = Item(id)
+        this.item.addDependency(anItem)
+    }
+    fun whichProvides(anItem:Item) {
+        this.item.addProvision(anItem)
+    }
+    fun whichUsesAcid(acidBuilder:AcidBuilder.()->Unit) {
+        val acid = Acid()
+        this.item.addUsage(acid)
+        AcidBuilder(acid).apply(acidBuilder)
+    }
+
+}
+
+class AcidBuilder(val acid:Acid) {
+    fun withType(type:String) {
+        acid.type = type
+    }
+    fun withGrade(grade:Int) {
+        acid.grade = grade
+    }
+}
---
+++ challenge33/src/test/kotlin/TestKotlinDSL.kt
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TestKotlinDSL {
+    @Test
+    fun `creating a config using configBuilder DSL`() {
+        val configuration = kotlinDSL()
+
+        val secure_air_vent =  configuration["secure_air_vent"]
+        assertEquals(0, secure_air_vent.uses.size)
+        assertEquals(0, secure_air_vent.provisions.size)
+        assertEquals(0, secure_air_vent.dependencies.size)
+
+        val acid_bath =  configuration["acid_bath"]
+        assertEquals(2, acid_bath.uses.size)
+
+        assertEquals(true, acid_bath.uses[0] is Acid)
+        val acid = acid_bath.uses[0] as Acid
+        assertEquals("hcl", acid.type)
+        assertEquals(5, acid.grade)
+
+        assertEquals(true, acid_bath.uses[1] is Electricity)
+        val acidBathElectricity = acid_bath.uses[1] as Electricity
+        assertEquals(12, acidBathElectricity.power)
+    }
+
+}
