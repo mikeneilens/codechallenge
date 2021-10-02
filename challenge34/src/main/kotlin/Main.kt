72e609091c77c1ef9e53081c1717ca9ec11b36ce
Mike Neilens <mike.neilens@gmail.com>
2021-10-02 16:23:04 +0100

    challenge34 in kotlin and swift

--- challenge34/src/main/kotlin/Main.kt
+++ challenge34/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 
 fun <Key, Value> memoize(function:(Key)->Value, cache:MutableMap<Key, Value> = mutableMapOf()): (Key)->Value {
-    return fun(p:Key):Value {
-        return if (cache[p] != null ) cache.getValue(p)
+    return  { p:Key ->
+        if (cache[p] != null ) cache.getValue(p)
         else {
             val result = function(p)
             cache[p] = result
