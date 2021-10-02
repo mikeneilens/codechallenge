093e23eff061a2ece3c27e643c60624e4eb798ef
Mike Neilens <mike.neilens@gmail.com>
2021-10-02 16:18:14 +0100

    challenge34 in kotlin and swift

--- challenge34/memoize.playground/Contents.swift
+++ challenge34/memoize.playground/Contents.swift
@@ -24,13 +24,12 @@ func  memoize<Key:Hashable, Value> (_ function: @escaping (Key) -> Value, using 
 class MemoizeTests: XCTestCase {
 
     let numberToString:(Int)->String = { n in String(n)}
-
+    
     func testWithDefaultCache() {
         let functionWithMemory = memoize(numberToString)
         XCTAssertEqual("56", functionWithMemory(56))
         XCTAssertEqual("61", functionWithMemory(61))
     }
-    
     func testUsingTestCache() {
         let testCache = Cache<Int, String>()
         let functionWithMemory = memoize(numberToString, using:testCache)
@@ -44,6 +43,16 @@ class MemoizeTests: XCTestCase {
         testCache[56] = "The wrong answer!"
         XCTAssertEqual("The wrong answer!", functionWithMemory(56))
     }
+    func testThatResultsAreAlwaysCached() {
+        func random(_ n:Int) -> Int {Int.random(in: 1..<n)}
+        let functionWithMemory = memoize(random)
+        let firstResult = functionWithMemory(100000)
+        let result2 = functionWithMemory(100000)
+        let result3 = functionWithMemory(100000)
+        XCTAssertEqual(firstResult, result2) //This could fail, although its very unlikely
+        XCTAssertEqual(firstResult, result3) //This could fail, although its very unlikely
+    }
+
 }
 MemoizeTests.defaultTestSuite.run()
 
