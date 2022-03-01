9a87d3ea075d212569283a7e227af411f141b56e
Mike Neilens <mike.neilens@gmail.com>
2022-03-01 17:26:22 +0000

    challenge35

--- challenge35/src/main/kotlin/Main.kt
+++ challenge35/src/main/kotlin/Main.kt
@@ -1,23 +1,22 @@
-typealias LetterChecker = (letter: Char, answerLetter: Char, numberOfEachLetter: MutableMap<Char, Int>) -> Boolean
+typealias LetterChecker = (guessLetter: Char, answerLetter: Char, qtyOfEachLetter: MutableMap<Char, Int>) -> Boolean
 
-val isGreen = {letter: Char, answerLetter: Char, _: MutableMap<Char, Int> -> letter == answerLetter}
+val isGreen = {guessLetter: Char, answerLetter: Char, _: MutableMap<Char, Int> -> guessLetter == answerLetter}
 
-val isYellow = {letter: Char, answerLetter: Char, numberOfEachLetter: MutableMap<Char, Int> ->
-    ( letter != answerLetter ) && ( letter in numberOfEachLetter ) && ( numberOfEachLetter.getValue(letter) > 0 ) }
-
+val isYellow = {guessLetter: Char, answerLetter: Char, qtyOfEachLetter: MutableMap<Char, Int> ->
+    ( guessLetter != answerLetter ) && ( guessLetter in qtyOfEachLetter ) && ( qtyOfEachLetter.getValue(guessLetter) > 0 ) }
 
 fun wordleResult(guess: String, answer: String):List<String> {
     val result = guess.map{"grey"}.toMutableList()
-    val numberOfEachLetter = answer.groupingBy { it }.eachCount().toMutableMap()
-    updateResult("green", guess, answer, numberOfEachLetter, result, isGreen)
-    updateResult("yellow", guess, answer, numberOfEachLetter, result, isYellow)
+    val qtyOfEachLetter = answer.groupingBy { it }.eachCount().toMutableMap()
+    updateResult("green", guess, answer, qtyOfEachLetter, result, isGreen)
+    updateResult("yellow", guess, answer, qtyOfEachLetter, result, isYellow)
     return result
 }
 
-private fun updateResult(colour: String, guess: String, answer: String, numberOfEachLetter: MutableMap<Char, Int>, result: MutableList<String>, isValidLetter: LetterChecker) {
-    guess.forEachIndexed { index, letter ->
-        if (isValidLetter(letter, answer[index], numberOfEachLetter)) {
-            numberOfEachLetter[letter] = numberOfEachLetter.getValue(letter) - 1
+private fun updateResult(colour: String, guess: String, answer: String, qtyOfEachLetter: MutableMap<Char, Int>, result: MutableList<String>, isValidLetter: LetterChecker) {
+    guess.forEachIndexed { index, guessLetter ->
+        if (isValidLetter(guessLetter, answer[index], qtyOfEachLetter)) {
+            qtyOfEachLetter[guessLetter] = qtyOfEachLetter.getValue(guessLetter) - 1
             result[index] = colour
         }
     }
