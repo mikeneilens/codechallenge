a92285e5bf5e63f2d941d585c39ceea87ff391c2
Mike Neilens <mike.neilens@gmail.com>
2022-03-02 15:05:14 +0000

    challenge35

--- challenge35/src/main/kotlin/Main.kt
+++ challenge35/src/main/kotlin/Main.kt
@@ -6,10 +6,10 @@ val isYellow = {guessLetter: Char, answerLetter: Char, qtyOfEachLetter: Map<Char
     ( guessLetter != answerLetter ) && ( guessLetter in qtyOfEachLetter ) && ( qtyOfEachLetter.getValue(guessLetter) > 0 ) }
 
 fun wordleResult(guess: String, answer: String):List<String> {
-    val result = guess.map{"grey"}.toMutableList()
+    val result = guess.map{"GREY"}.toMutableList()
     val qtyOfEachLetter = answer.groupingBy { it }.eachCount().toMutableMap()
-    updateResult("green", guess, answer, qtyOfEachLetter, result, isGreen)
-    updateResult("yellow", guess, answer, qtyOfEachLetter, result, isYellow)
+    updateResult("GREEN", guess, answer, qtyOfEachLetter, result, isGreen)
+    updateResult("YELLOW", guess, answer, qtyOfEachLetter, result, isYellow)
     return result
 }
 
--- challenge35/src/test/kotlin/MainTest.kt
+++ challenge35/src/test/kotlin/MainTest.kt
@@ -5,22 +5,22 @@ class MainTest: StringSpec({
     "two empty strings returns an empty list" ({
         wordleResult("","") shouldBe listOf<String>()
     })
-    "if guess contains no correct letters then return a list of grey"() {
-        wordleResult("abcd","efgh") shouldBe listOf<String>("grey","grey","grey","grey")
+    "if guess contains no correct letters then return a list of GREY"() {
+        wordleResult("abcd","efgh") shouldBe listOf<String>("GREY","GREY","GREY","GREY")
     }
-    "if guess contains a correct letter in the correct position then return a list containing green at the position and grey for everything else"() {
-        wordleResult("abcd","efch") shouldBe listOf<String>("grey","grey","green","grey")
+    "if guess contains a correct letter in the correct position then return a list containing GREEN at the position and GREY for everything else"() {
+        wordleResult("abcd","efch") shouldBe listOf<String>("GREY","GREY","GREEN","GREY")
     }
-    "if guess contains a correct letter in the incorrect position then return a list containing yellow at the position and grey for everything else"() {
-        wordleResult("abcd","ecgh") shouldBe listOf<String>("grey","grey","yellow","grey")
+    "if guess contains a correct letter in the incorrect position then return a list containing YELLOW at the position and GREY for everything else"() {
+        wordleResult("abcd","ecgh") shouldBe listOf<String>("GREY","GREY","YELLOW","GREY")
     }
-    "if guess contains a correct letter in the correct position and in the incorrect position then return a list containing green at the correct position and grey for everything else"() {
-        wordleResult("accd","ecgh") shouldBe listOf<String>("grey","green","grey","grey")
+    "if guess contains a correct letter in the correct position and in the incorrect position then return a list containing GREEN at the correct position and GREY for everything else"() {
+        wordleResult("accd","ecgh") shouldBe listOf<String>("GREY","GREEN","GREY","GREY")
     }
-    "if guess contains a correct letter twice in the the incorrect positions then return a list containing yellow at the first occurence and grey for everything else"() {
-        wordleResult("cacd","ecgh") shouldBe listOf<String>("yellow","grey","grey","grey")
+    "if guess contains a correct letter in the the incorrect positions then return a list containing YELLOW at the first occurence and GREY for everything else"() {
+        wordleResult("cacd","ecgh") shouldBe listOf<String>("YELLOW","GREY","GREY","GREY")
     }
-    "if guess contains a correct letter two times but incorrect position both times then a list containing two yellows and grey for everything else"() {
-        wordleResult("cacd","ecgc") shouldBe listOf<String>("yellow","grey","yellow","grey")
+    "if guess contains a correct letter two times but incorrect position both times then a list containing two YELLOWs and GREY for everything else"() {
+        wordleResult("cacd","ecgc") shouldBe listOf<String>("YELLOW","GREY","YELLOW","GREY")
     }
 })
