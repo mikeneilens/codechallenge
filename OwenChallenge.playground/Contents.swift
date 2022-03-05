96c68755480465579101a0bb64d8ab56f7ac94a8
Mike Neilens <mike.neilens@gmail.com>
2022-03-05 15:16:41 +0000

    owens challenge

--- OwenChallenge.playground/Contents.swift
+++ OwenChallenge.playground/Contents.swift
@@ -6,17 +6,21 @@ struct Owen{
     
     func plus(_ s:String) -> Owen {
         switch (s) {
-        case "(": return  Owen(output:output + ((noOfBracketsOpen == 0) ? [""] : []) , noOfBracketsOpen:noOfBracketsOpen + 1)
+        case "(": return Owen(output:output + ((noOfBracketsOpen == 0) ? [""] : []) , noOfBracketsOpen:noOfBracketsOpen + 1)
         case ")": return Owen(output:output, noOfBracketsOpen:noOfBracketsOpen - 1)
         default:
             if noOfBracketsOpen > 0 {
-                return Owen(output: output.dropLast() + ["\( (output.last ?? "") + s)"], noOfBracketsOpen: noOfBracketsOpen)
+                return Owen(output: output.concatonateLastStringWith(s), noOfBracketsOpen: noOfBracketsOpen)
             } else {
                 return Owen(output: output + [s], noOfBracketsOpen: 0)
             }
         }
     }
  }
+
+extension Array where Element==String {
+    func concatonateLastStringWith(_ s:String) -> Array<String> { dropLast() + [(last ?? "") + s]}
+}
 
 func solution(_ data: Array<String>) -> Array<String> {
     data.reduce( Owen(output:[], noOfBracketsOpen: 0)){owen, string in owen.plus(string)}.output
