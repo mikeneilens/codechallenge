9c5b21945bfa326a79f97ed8e080d78c36011e2c
Mike Neilens <mike.neilens@gmail.com>
2022-03-05 15:08:02 +0000

    owens challenge

--- OwenChallenge.playground/Contents.swift
+++ OwenChallenge.playground/Contents.swift
@@ -2,24 +2,24 @@ import XCTest
 
 struct Owen{
     let output:Array<String>
-    let shouldConcat:Bool
+    let noOfBracketsOpen:Int
     
     func plus(_ s:String) -> Owen {
         switch (s) {
-        case "(": return Owen(output:output + [""] , shouldConcat:true)
-        case ")": return Owen(output:output , shouldConcat:false)
+        case "(": return  Owen(output:output + ((noOfBracketsOpen == 0) ? [""] : []) , noOfBracketsOpen:noOfBracketsOpen + 1)
+        case ")": return Owen(output:output, noOfBracketsOpen:noOfBracketsOpen - 1)
         default:
-            if shouldConcat {
-                return Owen(output: output.dropLast() + ["\( (output.last ?? "") + s)"], shouldConcat:true)
+            if noOfBracketsOpen > 0 {
+                return Owen(output: output.dropLast() + ["\( (output.last ?? "") + s)"], noOfBracketsOpen: noOfBracketsOpen)
             } else {
-                return Owen(output: output + [s], shouldConcat:false)
+                return Owen(output: output + [s], noOfBracketsOpen: 0)
             }
         }
     }
-}
+ }
 
 func solution(_ data: Array<String>) -> Array<String> {
-    data.reduce( Owen(output:[], shouldConcat:false)){owen, string in owen.plus(string)}.output
+    data.reduce( Owen(output:[], noOfBracketsOpen: 0)){owen, string in owen.plus(string)}.output
 }
 
 class OwenTests: XCTestCase {
@@ -34,6 +34,9 @@ class OwenTests: XCTestCase {
     }
     func testWithTwoSetsOfBrackets() {
         XCTAssertEqual(solution(["a","b","(","a","b",")","a","b","(","a","b",")"]) , ["a","b","ab","a","b","ab"])
+    }
+    func testWithBracketsNestedInBrackets() {
+        XCTAssertEqual(solution(["a","(","b","(","c","d",")","e",")","f"]), ["a", "bcde", "f"])
     }
 }
 
