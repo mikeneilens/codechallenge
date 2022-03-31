e0ebb6f8ac5a111c625041612bb643d352683002
Mike Neilens <mike.neilens@gmail.com>
2022-03-31 19:00:47 +0100

    update to challenge32

--- challenge32/src/main/kotlin/Rates.kt
+++ challenge32/src/main/kotlin/Rates.kt
@@ -1,10 +1,3 @@
-//const val MON_TO_THU_RATE_A = 25
-//const val WEEKEND_RATE_A = 40
-//const val BANK_HOLIDAY_RATE_A = 80
-//const val MON_TO_THU_RATE_B = 15
-//const val WEEKEND_RATE_B = 25
-//const val BANK_HOLIDAY_RATE_B = 50
-
 const val MON_TO_FRI_RATE_A = 35
 const val WEEKEND_RATE_A = 56
 const val BANK_HOLIDAY_RATE_A = 112
