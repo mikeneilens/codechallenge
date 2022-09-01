ed666848135f6290f95828f9cea5d4be807dd69f
Mike Neilens <mike.neilens@gmail.com>
2022-09-01 14:40:51 +0100

    challenge37

--- challenge37/src/main/kotlin/Main.kt
+++ challenge37/src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
+typealias Lunches = Set<Fruit>
 
-fun possibleLunch(fruits:List<Fruit>, result:Set<Set<Fruit>> = emptySet()):Set<Set<Fruit>> =
+fun possibleLunch(fruits:List<Fruit>, result:Set<Lunches> = emptySet()):Set<Lunches> =
     if (fruits.isEmpty()) result
     else {
-        val lunches = result + result.map{ it + fruits.first()} + setOf(setOf(fruits.first())) 
+        val lunches = result.addFruit(fruits.first())
         possibleLunch(fruits.drop(1), lunches)
     }
+
+private fun Set<Lunches>.addFruit(fruit: Fruit) = this + map { it + fruit } + setOf(setOf(fruit))
