e42899ddbcf609f21cda8386cf02c009fa281c5f
Mike Neilens <mike.neilens@gmail.com>
2022-09-01 14:32:11 +0100

    challenge 37

--- challenge37/src/main/kotlin/Main.kt
+++ challenge37/src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 
-fun possibleLunch(fruits:List<Fruit>, result:Set<Set<Fruit>> = setOf()):Set<Set<Fruit>> =
+fun possibleLunch(fruits:List<Fruit>, result:Set<Set<Fruit>> = emptySet()):Set<Set<Fruit>> =
     if (fruits.isEmpty()) result
     else {
         val lunches = result + result.map{ it + fruits.first()} + setOf(setOf(fruits.first())) 
