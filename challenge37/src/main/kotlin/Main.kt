fa37c4dd7f176be199b8f7305e72e318de59d1dc
Mike Neilens <mike.neilens@gmail.com>
2022-09-02 10:52:32 +0100

    challenge 37 optimised version

--- challenge37/src/main/kotlin/Main.kt
+++ challenge37/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 typealias Lunches = Set<Fruit>
 
 fun possibleLunch(fruits:List<Fruit>, result:Set<Lunches> = emptySet()):Set<Lunches> =
@@ -8,3 +10,18 @@ fun possibleLunch(fruits:List<Fruit>, result:Set<Lunches> = emptySet()):Set<Lunc
     }
 
 private fun Set<Lunches>.addFruit(fruit: Fruit) = this + map { it + fruit } + setOf(setOf(fruit))
+
+//optimised version
+fun possibleLunch2(fruits:List<Fruit>):List<List<Fruit>> {
+    val result = mutableListOf<List<Fruit>>()
+    (1..fruits.numberOfCombinations).forEach { number ->
+        val binary = number.toString(2)
+        val lunch =  binary.mapIndexedNotNull{digit, char -> fruitForDigit(digit, binary.length, char, fruits)}
+        result.add(lunch)
+    }
+    return result
+}
+
+val List<Fruit>.numberOfCombinations get() = (2.0.pow(size) - 1).toInt()
+
+fun fruitForDigit(digit:Int, binaryLength:Int, digitValue:Char, fruits:List<Fruit>) = if (digitValue == '1') fruits[fruits.size - binaryLength + digit] else null
--- challenge37/src/test/kotlin/MainTest.kt
+++ challenge37/src/test/kotlin/MainTest.kt
@@ -1,7 +1,6 @@
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-
 class MainTest: StringSpec({
 
     "An empty list of fruit returns an empty set of lunches" {
@@ -51,5 +50,36 @@ class MainTest: StringSpec({
     "A list of twenty fruit generates 1048575 different combinations" {
         possibleLunch(listOf(Apple, Orange, Pear, Grapes, Nectarine, Strawberries, Clementine, Plums, Blueberries, Banana, Melon, Mango, Kiwifriut, Pomegranate, Apricot, Coconut, Grapefruit, Tangerine, Cherries, Tomato)).size shouldBe 1048575
     }
+
+    "fruit of first digit of '10' when there are 5 fruits is Grapes" {
+        fruitForDigit(0, "10".length, '1', listOf(Apple, Orange, Pear, Grapes, Nectarine)) shouldBe Grapes
+    }
+    "fruit of second digit of '11' when there are 5 fruits is Nectarine" {
+        fruitForDigit(1, "11".length, '1', listOf(Apple, Orange, Pear, Grapes, Nectarine)) shouldBe Nectarine
+    }
+    "fruit of second digit of '10' when there are 5 fruits is null" {
+        fruitForDigit(1, "10".length, '0', listOf(Apple, Orange, Pear, Grapes, Nectarine)) shouldBe null
+    }
+    "fruit of first digit of '100' when there are 5 fruits is Pear" {
+        fruitForDigit(0, "100".length, '1', listOf(Apple, Orange, Pear, Grapes, Nectarine)) shouldBe Pear
+    }
+
+    //optimised
+    "(Optimised) A list of one Apple and one Oranges and one Pear creates 7 combinations" {
+        possibleLunch2(listOf(Apple, Orange, Pear)) shouldBe
+                listOf(
+                    listOf(Pear),
+                    listOf(Orange),
+                    listOf(Orange, Pear),
+                    listOf(Apple),
+                    listOf(Apple, Pear),
+                    listOf(Apple, Orange),
+                    listOf(Apple, Orange, Pear))
+    }
+    "(Optimised) A list of twenty fruit generates 1048575 different combinations" {
+        possibleLunch2(listOf(Apple, Orange, Pear, Grapes, Nectarine, Strawberries, Clementine, Plums, Blueberries, Banana, Melon, Mango, Kiwifriut, Pomegranate, Apricot, Coconut, Grapefruit, Tangerine, Cherries, Tomato)).size shouldBe 1048575
+    }
+
+
 })
 
